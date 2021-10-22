54453da44ef8c860df4d0433f80e27f7ba3db4af
Oscar <oscarknap@ziggo.nl>
2021-10-22 09:41:04 +0200

    [NW] [FB]

--- src/functions/loader/tests.js
+++ src/functions/loader/tests.js
@@ -4,10 +4,11 @@ let files = [];
 
 fs.readdirSync(path.resolve(__dirname, '../../classes/exports/')).forEach(file => {
     if (file.split('.').length == 2)
-        files.push({
-            class: file.split('.js')[0],
-            test: require(`../../classes/exports/${file.split('.js')[0]}.test.js`)
-        })
+        if (fs.existsSync(path.resolve(__dirname, `../../classes/exports/${file.split('.js')[0]}.test.js`)))
+            files.push({
+                class: file.split('.js')[0],
+                test: require(`../../classes/exports/${file.split('.js')[0]}.test.js`)
+            })
 })
 
 fs.readdirSync(path.resolve(__dirname, '../../classes/utils/')).forEach(file => {
