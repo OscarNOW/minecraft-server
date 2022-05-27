41b39e7f18679057ac51aee8ec1a8da253836799
Oscar <oscarknap@ziggo.nl>
2022-05-27 22:42:57 +0200

    Moved title to seperate file

--- src/classes/utils/Client.js
+++ src/classes/utils/Client.js
@@ -512,34 +512,6 @@ class Client extends EventEmitter {
         })
     }
 
-    title(properties) {
-        if (!this[this.ps.canUsed])
-            throw new Error(`This action can't be performed on this Client right now. ${this.online ? 'This may be because the Client is no longer online or that the client is not ready to receive this packet.' : 'This is because the Client is no longer online'}`)
-
-        let { fadeIn, stay, fadeOut, title, subTitle } = properties || {};
-
-        this[this.ps.sendPacket]('title', {
-            action: 5
-        })
-
-        this[this.ps.sendPacket]('title', {
-            action: 3,
-            fadeIn: fadeIn ?? 10,
-            stay: stay ?? 40,
-            fadeOut: fadeOut ?? 10
-        })
-
-        this[this.ps.sendPacket]('title', {
-            action: 0,
-            text: JSON.stringify({ translate: `${(title && title !== '') ? title : ''}` })
-        })
-        if (subTitle && subTitle !== '')
-            this[this.ps.sendPacket]('title', {
-                action: 1,
-                text: JSON.stringify({ translate: `${subTitle}` })
-            })
-    }
-
     actionBar(text) {
         if (!this[this.ps.canUsed])
             throw new Error(`This action can't be performed on this Client right now. ${this.online ? 'This may be because the Client is no longer online or that the client is not ready to receive this packet.' : 'This is because the Client is no longer online'}`)
