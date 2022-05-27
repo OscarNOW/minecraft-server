c04a8fbfc2e9d6a90da146f7c8dbc2ada26cb138
Oscar <oscarknap@ziggo.nl>
2022-05-27 23:42:00 +0200

    Moved player function to seperate file

---
+++ src/classes/utils/Client/player.js
@@ -0,0 +1,29 @@
+module.exports = {
+    player: function () {
+        throw new Error(`Not implemented`)
+        if (!this[this.ps.canUsed])
+            throw new Error(`This action can't be performed on this Client right now. ${this.online ? 'This may be because the Client is no longer online or that the client is not ready to receive this packet.' : 'This is because the Client is no longer online'}`)
+
+        /*httpRequest({
+            host: 'sessionserver.mojang.com',
+            method: 'GET',
+            path: `/session/minecraft/profile/${this.uuid}?unsigned=false`
+        }).then(inf => {
+            console.log(inf.properties)*/
+        this[this.ps.sendPacket]('player_info', {
+            action: 0,
+            data: [
+                {
+                    UUID: this.uuid,
+                    name: this.username,
+                    // properties: inf.properties,
+                    properties: [],
+                    gamemode: 0,
+                    ping: this.ping,
+                    displayName: this.username
+                }
+            ]
+        })
+        // })
+    }
+}
