3306140614011edddd3cff406baa1995142ea87f
Oscar <oscarknap@ziggo.nl>
2022-06-04 15:01:39 +0200

    Moved Client dynamic gamemode property into seperate file

--- src/classes/utils/Client.js
+++ src/classes/utils/Client.js
@@ -350,23 +350,11 @@ class Client extends EventEmitter {
     }
 
     get gamemode() {
-        return this[this.ps._gamemode]
+        return this[this.ps.get]['gamemode']();
     }
 
-    set gamemode(gamemode) {
-        if (!this[this.ps.canUsed])
-            throw new Error(`This action can't be performed on this Client right now. ${this.online ? 'This may be because the Client is no longer online or that the client is not ready to receive this packet.' : 'This is because the Client is no longer online'}`)
-
-        if (!['survival', 'creative', 'adventure', 'spectator'].includes(gamemode))
-            throw new Error(`Unknown gamemode "${gamemode}" (${typeof gamemode})`)
-
-        this[this.ps.sendPacket]('game_state_change', {
-            reason: 3,
-            gameMode: ['survival', 'creative', 'adventure', 'spectator'].indexOf(gamemode)
-        })
-
-        this[this.ps._gamemode] = gamemode;
-        this[this.ps.emitObservable]('gamemode');
+    set gamemode(v) {
+        return this[this.ps.set]['gamemode'](v);
     }
 
     get difficulty() {
