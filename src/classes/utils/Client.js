11834a5d08b15674c09c0f26b873b471cff0e680
Oscar <oscarknap@ziggo.nl>
2022-06-04 14:52:27 +0200

    Moved Client dynamic slot property into seperate file

--- src/classes/utils/Client.js
+++ src/classes/utils/Client.js
@@ -334,19 +334,11 @@ class Client extends EventEmitter {
     }
 
     get slot() {
-        return this[this.ps._slot]
+        return this[this.ps.get]['slot']();
     }
 
-    set slot(slot) {
-        if (!this[this.ps.canUsed])
-            throw new Error(`This action can't be performed on this Client right now. ${this.online ? 'This may be because the Client is no longer online or that the client is not ready to receive this packet.' : 'This is because the Client is no longer online'}`)
-
-        if (isNaN(parseInt(slot)) || slot < 0 || slot > 8)
-            throw new Error(`Unknown slot, expected an integer between 0 and 8, received "${slot}" (${typeof slot})`)
-
-        this[this.ps.sendPacket]('held_item_slot', {
-            slot: parseInt(slot)
-        })
+    set slot(v) {
+        return this[this.ps.set]['slot'](v);
     }
 
     get darkSky() {
