5c336324a7a00643e9ae7e39cd579cf34121e6a0
Oscar <oscarknap@ziggo.nl>
2022-06-05 20:22:14 +0200

    Moved client.updateCanUsed into separate file

--- src/classes/utils/Client.js
+++ src/classes/utils/Client.js
@@ -54,28 +54,6 @@ class Client extends EventEmitter {
             this.p.updateCanUsed();
         });
 
-        this.p.updateCanUsed = () => {
-            this.p.readyStates.socketOpen = this.online;
-            let canUsed = true;
-            for (const val of Object.values(this.p.readyStates))
-                if (!val) canUsed = false;
-
-            this.p.canUsed = canUsed;
-            if (this.p.canUsed && !this.p.joinedPacketSent && !this.p.leftPacketSent) {
-                this.p.joinedPacketSent = true;
-
-                this.server.clients.push(this);
-                this.server.emit('join', this);
-
-            } else if (!canUsed && !this.p.leftPacketSent && this.p.joinedPacketSent) {
-                this.p.leftPacketSent = true;
-
-                this.server.clients = this.server.clients.filter(client => client.canUsed);
-                this.emit('leave');
-                this.server.emit('leave', this);
-            }
-        }
-
         //Inject private methods
         Object.entries(
             Object.assign({}, ...fs
---
+++ src/classes/utils/Client/methods/private/updateCanUsed.js
@@ -0,0 +1,23 @@
+module.exports = {
+    updateCanUsed: function () {
+        this.p.readyStates.socketOpen = this.online;
+        let canUsed = true;
+        for (const val of Object.values(this.p.readyStates))
+            if (!val) canUsed = false;
+
+        this.p.canUsed = canUsed;
+        if (this.p.canUsed && !this.p.joinedPacketSent && !this.p.leftPacketSent) {
+            this.p.joinedPacketSent = true;
+
+            this.server.clients.push(this);
+            this.server.emit('join', this);
+
+        } else if (!canUsed && !this.p.leftPacketSent && this.p.joinedPacketSent) {
+            this.p.leftPacketSent = true;
+
+            this.server.clients = this.server.clients.filter(client => client.canUsed);
+            this.emit('leave');
+            this.server.emit('leave', this);
+        }
+    }
+}
