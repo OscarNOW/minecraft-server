c83a560b7bd70218279ca3f30da6d041dd440436
Oscar <oscarknap@ziggo.nl>
2022-06-06 21:10:56 +0200

    Implemented teleport_confirm check

---
+++ src/classes/utils/Client/events/teleport_confirm.js
@@ -0,0 +1,7 @@
+const { position: { confirm } } = require('../properties/public/dynamic/position');
+
+module.exports = {
+    teleport_confirm: function ({ teleportId }) {
+        confirm.call(this, teleportId)
+    }
+}
--- src/classes/utils/Client/properties/public/dynamic/position.js
+++ src/classes/utils/Client/properties/public/dynamic/position.js
@@ -1,4 +1,6 @@
-const { ChangablePosition } = require('../../../../ChangablePosition.js')
+const { ChangablePosition } = require('../../../../ChangablePosition.js');
+
+const teleportPromises = new Map();
 
 module.exports = {
     position: {
@@ -9,14 +11,34 @@ module.exports = {
             if (!this.p.canUsed)
                 throw new Error(`This action can't be performed on this Client right now. ${this.online ? 'This may be because the Client is no longer online or that the client is not ready to receive this packet.' : 'This is because the Client is no longer online'}`)
 
+            const teleportId = Math.floor(Math.random() * 1000);
+            const promise = new Promise((res, rej) => {
+                let obj = teleportPromises.get(this) || {};
+                obj[teleportId] = {
+                    res,
+                    rej,
+                    resolved: false
+                }
+
+                teleportPromises.set(this, obj)
+
+                setTimeout(() => {
+                    if (!teleportPromises.get(this)[teleportId].resolved)
+                        rej(new Error(`Client didn't send teleport confirm after sending client teleport`))
+                }, 5000)
+            })
+
             this.p.sendPacket('position', {
                 x: x ?? this.position.x,
                 y: y ?? this.position.y,
                 z: z ?? this.position.z,
                 yaw: yaw ?? this.position.yaw,
                 pitch: pitch ?? this.position.pitch,
-                flags: 0x00
+                flags: 0x00,
+                teleportId
             });
+
+            return promise;
         },
         init: function () {
             this.p._position = new ChangablePosition((function (i) { this.position = i }).bind(this), {
@@ -26,6 +48,10 @@ module.exports = {
                 yaw: null,
                 pitch: null
             });
+        },
+        confirm: function (teleportId) {
+            teleportPromises.get(this)[teleportId].resolved = true
+            teleportPromises.get(this)[teleportId].res()
         }
     }
 }
