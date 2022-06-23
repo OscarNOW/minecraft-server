4ba36c4e39fc486575e9ec8898519944cc4b3b88
Oscar <oscarknap@ziggo.nl>
2022-06-23 19:19:53 +0200

    Bug fix

--- src/docs/generateAfter.js
+++ src/docs/generateAfter.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const path = require('path');
 
 let customCss = fs.readFileSync(path.resolve(__dirname, '../../docs/assets/custom.css')).toString()
-customCss = customCss.replace(/background-image: url(26e93147f10415a0ed4a.svg)/g, '');
+customCss = customCss.replace('.category__link--ts', '.none');
 fs.writeFileSync(path.resolve(__dirname, '../../docs/assets/custom.css'), customCss);
