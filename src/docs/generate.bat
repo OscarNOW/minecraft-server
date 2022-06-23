c5b6cd490ad724f7fe1d721a75bcc4ccc20357c8
Oscar <oscarknap@ziggo.nl>
2022-06-23 17:25:54 +0200

    Updated docs

--- src/docs/generate.bat
+++ src/docs/generate.bat
@@ -18,4 +18,4 @@ pause
 cls
 
 echo Generating documentation...
-typedoc ./index.d.ts --readme ./Readme.md --out ../../docs/ --plugin ../../node_modules/typedoc-theme-hierarchy/dist/index.js --theme hierarchy --name "minecraft-server" --disableSources
+typedoc ./index.d.ts --readme ./Readme.md --out ../../docs/ --plugin ../../node_modules/typedoc-theme-hierarchy/dist/index.js --theme hierarchy --name "minecraft-server" --disableSources --sort visibility --sort static-first --sort alphabetical --gitRemote origin --hideGenerator
