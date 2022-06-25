c4e88196fd8918e7bce02f4b2c5c4c840b14256e
Oscar <oscarknap@ziggo.nl>
2022-06-25 23:02:43 +0200

    Made health defaultable

--- src/classes/utils/Client/properties/public/dynamic/health.js
+++ src/classes/utils/Client/properties/public/dynamic/health.js
@@ -1,28 +1,45 @@
 module.exports = {
     health: {
+        info: {
+            callAfterLogin: true
+        },
         get: function () {
             return this.p._health
         },
-        set: function (h) {
+        set: function (v) {
             if (!this.p.canUsed)
                 if (this.online)
                     throw new Error(`This action can't be performed on this Client right now. This may be because the Client is no longer online or that the client is not ready to receive this packet.`)
                 else
                     throw new Error(`Can't perform this action on an offline player`)
 
-            const health = parseInt(h);
+            const value = parseInt(v);
 
-            if (isNaN(health) || health < 0 || health > 20)
-                throw new Error(`Unknown health, expected an integer between 0 and 20, received "${h}" (${typeof h})`)
+            if (isNaN(value) || value < 0 || value > 20)
+                throw new Error(`Unknown health, expected an integer between 0 and 20, received "${v}" (${typeof v})`)
 
             this.p.sendPacket('update_health', {
-                health,
+                health: value,
                 food: this.p._food,
                 foodSaturation: this.p._foodSaturation
             })
 
-            this.p._health = health;
+            this.p._health = value;
             this.p.emitObservable('health');
+        },
+        setRaw: function (v) {
+            const value = parseInt(v);
+
+            if (isNaN(value) || value < 0 || value > 20)
+                throw new Error(`Unknown health, expected an integer between 0 and 20, received "${v}" (${typeof v})`)
+
+            this.p.sendPacket('update_health', {
+                health: value,
+                food: this.p._food,
+                foodSaturation: this.p._foodSaturation
+            })
+
+            this.p._health = value;
         },
         init: function () {
             this.p._health = 20;
