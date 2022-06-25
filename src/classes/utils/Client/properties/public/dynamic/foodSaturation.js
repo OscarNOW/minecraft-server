eb7e67fc5d8df04efd05eb6c17271fa6c0cc8def
Oscar <oscarknap@ziggo.nl>
2022-06-25 21:16:25 +0200

    Made foodSaturation defaultable

--- src/classes/utils/Client/properties/public/dynamic/foodSaturation.js
+++ src/classes/utils/Client/properties/public/dynamic/foodSaturation.js
@@ -1,28 +1,45 @@
 module.exports = {
     foodSaturation: {
+        info: {
+            callAfterLogin: true
+        },
         get: function () {
             return this.p._foodSaturation
         },
-        set: function (fs) {
+        set: function (v) {
             if (!this.p.canUsed)
                 if (this.online)
                     throw new Error(`This action can't be performed on this Client right now. This may be because the Client is no longer online or that the client is not ready to receive this packet.`)
                 else
                     throw new Error(`Can't perform this action on an offline player`)
 
-            const foodSaturation = parseInt(fs);
+            const value = parseInt(v);
 
-            if (isNaN(foodSaturation) || foodSaturation < 0 || foodSaturation > 5)
-                throw new Error(`Unknown foodSaturation, expected an integer between 0 and 5, received "${fs}" (${typeof fs})`)
+            if (isNaN(value) || value < 0 || value > 5)
+                throw new Error(`Unknown foodSaturation, expected an integer between 0 and 5, received "${v}" (${typeof v})`)
 
             this.p.sendPacket('update_health', {
                 health: this.p._health,
                 food: this.p._food,
-                foodSaturation
+                foodSaturation: value
             })
 
-            this.p._foodSaturation = foodSaturation;
+            this.p._foodSaturation = value;
             this.p.emitObservable('foodSaturation');
+        },
+        setRaw(v) {
+            const value = parseInt(v);
+
+            if (isNaN(value) || value < 0 || value > 5)
+                throw new Error(`Unknown foodSaturation, expected an integer between 0 and 5, received "${v}" (${typeof v})`)
+
+            this.p.sendPacket('update_health', {
+                health: this.p._health,
+                food: this.p._food,
+                foodSaturation: value
+            })
+
+            this.p._foodSaturation = value;
         },
         init: function () {
             this.p._foodSaturation = 5;
