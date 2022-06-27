0ffd3028abfcdf6a47bb027befac7fc83a3e84ad
Oscar <oscarknap@ziggo.nl>
2022-06-27 09:05:01 +0200

    Changed styling back to original

--- src/docs/transformAfter.js
+++ src/docs/transformAfter.js
@@ -168,9 +168,9 @@ for (const [className, classData] of Object.entries(parsedExamples)) {
     fs.writeFileSync(path.resolve(__dirname, `../../docs/classes/${className}.html`), file);
 }
 
-console.log('Writing overwrites...')
-console.log('   style.css')
-fs.writeFileSync(path.resolve(__dirname, `../../docs/assets/style.css`), fs.readFileSync(path.resolve(__dirname, `./overwrites/style.css`)).toString());
+// console.log('Writing overwrites...')
+// console.log('   style.css')
+// fs.writeFileSync(path.resolve(__dirname, `../../docs/assets/style.css`), fs.readFileSync(path.resolve(__dirname, `./overwrites/style.css`)).toString());
 
 console.log('Done')
 
