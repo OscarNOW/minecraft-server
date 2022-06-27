08470139e5b3e05a293ad2d7d01a60177bb4c60d
Oscar <oscarknap@ziggo.nl>
2022-06-27 16:07:47 +0200

    Added client.observe example

---
+++ src/classes/utils/Client/methods/public/observe.examples.js
@@ -0,0 +1,8 @@
+module.exports = {
+    observe: [
+        {
+            code: `
+client.observe('slot', slot => console.log(\`Client switched slot to \${slot}\`))`
+        }
+    ]
+}
--- src/classes/utils/Client/properties/public/dynamic/online.examples.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    online: [
-        {
-            code: `
-console.log(client.online)
-`
-        }
-    ]
-}
