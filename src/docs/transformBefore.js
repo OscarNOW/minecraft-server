b3dfcfe1de829e20941930d1b6b82746a3eff6f3
Oscar <oscarknap@ziggo.nl>
2022-07-02 10:11:08 +0200

    Made docs work again

--- src/docs/transformBefore.js
+++ src/docs/transformBefore.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log('Transforming readme before...')
+console.log('Transforming readme...')
 
 let readme = fs.readFileSync(path.resolve(__dirname, './Readme.md')).toString()
 
@@ -15,68 +15,6 @@ readme[licensePosition] = `
 readme = readme.join('\n')
 
 readme = readme.replace(/\/assets\//g, '/minecraft-server/assets/');
-
-console.log('Getting examples...')
-let examples = {};
-
-examples = {
-    ...examples, ...Object.fromEntries(fs
-        .readdirSync(path.resolve(__dirname, '../classes/exports/'))
-        .filter(a => a.endsWith('.examples.js'))
-        .map(a => [a.split('.examples.js')[0],
-        Object.fromEntries(
-            Object.entries(
-                require(`../classes/exports/${a}`)
-            )
-                .map(([key, value]) => key == 'constructor' ? ['constructors', { constructor: value }] : [key, value])
-        )])
-    )
-}
-
-examples = {
-    ...examples, ...Object.fromEntries(fs
-        .readdirSync(path.resolve(__dirname, '../classes/utils/'))
-        .filter(a => a.endsWith('.examples.js'))
-        .map(a => [a.split('.examples.js')[0],
-        Object.fromEntries(
-            Object.entries(
-                require(`../classes/utils/${a}`)
-            )
-                .map(([key, value]) => key == 'constructor' ? ['constructors', { constructor: value }] : [key, value])
-        )])
-    )
-}
-
-readme += `
-
-
-
-;`
-
-console.log('Parsing examples...')
-let parsedExamples = [];
-
-for (const [k1, v1] of Object.entries(examples)) {
-    let t1 = `${k1}`;
-
-    ['methods', 'properties', 'constructors'].forEach(k2 => {
-        let v2 = v1[k2];
-        let t2 = `${t1}|${k2}`
-
-        if (v2)
-            for (const [k3, v3] of Object.entries(v2)) {
-                let t3 = `${t2}|${k3}`;
-
-                v3.forEach((v4, k4) => {
-                    let t4 = `${t3}|${k4}`;
-
-                    parsedExamples.push(`${t4}\n` + "```" + `js\n${v4.code}\n` + "```")
-                })
-            }
-    })
-}
-
-readme += parsedExamples.join('\n:')
 
 fs.writeFileSync(path.resolve(__dirname, './Readme.md'), readme);
 
