f5172702f15342819a06cc19c944572bfaa7039b
Oscar <oscarknap@ziggo.nl>
2022-07-18 13:07:38 +0100

    Tested realm translates

--- src/classes/utils/Client/methods/public/chat.js
+++ src/classes/utils/Client/methods/public/chat.js
@@ -8,7 +8,6 @@ module.exports = {
 
         this.p.sendPacket('chat', {
             message: JSON.stringify({ text: `${message}` }),
-            // message: JSON.stringify({ translate: 'mco.selectServer.leave' }),
             position: 0,
             sender: '0'
         });
