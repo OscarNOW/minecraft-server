b0e0e4f5715b2b14ed3a45cb05ec1ebd3b53aa78
Oscar <oscarknap@ziggo.nl>
2022-07-18 13:12:53 +0100

    Removed non existent keybind

--- src/docs/index.d.ts
+++ src/docs/index.d.ts
@@ -704,7 +704,7 @@ import { EventEmitter } from 'events';export class Chunk {
     text: string;
     color?: textColor;
     modifiers?: Array<textModifier>;
-};type keycode = 'key.jump'|'key.sneak'|'key.sprint'|'key.left'|'key.right'|'key.back'|'key.forward'|'key.attack'|'key.pickItem'|'key.use'|'key.drop'|'key.hotbar.1'|'key.hotbar.2'|'key.hotbar.3'|'key.hotbar.4'|'key.hotbar.5'|'key.hotbar.6'|'key.hotbar.7'|'key.hotbar.8'|'key.hotbar.9'|'key.inventory'|'key.swapOffhand'|'key.loadToolbarActivator'|'key.saveToolbarActivator'|'key.playerlist'|'key.chat'|'key.command'|'key.socialInteractions'|'key.advancements'|'key.spectatorOutlines'|'key.screenshot'|'key.smoothCamera'|'key.fullscreen'|'key.togglePerspective';type defaultClientProperties = {
+};type keycode = 'key.jump'|'key.sneak'|'key.sprint'|'key.left'|'key.right'|'key.back'|'key.forward'|'key.attack'|'key.pickItem'|'key.use'|'key.drop'|'key.hotbar.1'|'key.hotbar.2'|'key.hotbar.3'|'key.hotbar.4'|'key.hotbar.5'|'key.hotbar.6'|'key.hotbar.7'|'key.hotbar.8'|'key.hotbar.9'|'key.inventory'|'key.swapOffhand'|'key.loadToolbarActivator'|'key.saveToolbarActivator'|'key.playerlist'|'key.chat'|'key.command'|'key.advancements'|'key.spectatorOutlines'|'key.screenshot'|'key.smoothCamera'|'key.fullscreen'|'key.togglePerspective';type defaultClientProperties = {
     position?: {
         x?: number;
         y?: number;
