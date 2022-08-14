86620f451bd61a1e8fcbdc586b89355ea9700a31
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-08-14 12:58:17 +0200

    Updated minifier to also minify MC messages

--- src/minify.js
+++ src/minify.js
@@ -4,12 +4,22 @@ const path = require('path');
 console.clear();
 console.log('Minifieing JSON files')
 
-for (const file of fs
-    .readdirSync(path.resolve(__dirname, './data/'))
-    .filter(file => file.endsWith('.json'))
-) {
+for (const file of [
+    ...fs
+        .readdirSync(path.resolve(__dirname, './data/'))
+        .filter(file => file.endsWith('.json'))
+        .map(a => `./data/${a}`),
+    ...fs
+        .readdirSync(path.resolve(__dirname, './data/messages/game/'))
+        .filter(file => file.endsWith('.json'))
+        .map(a => `./data/messages/game/${a}`),
+    ...fs
+        .readdirSync(path.resolve(__dirname, './data/messages/realms/'))
+        .filter(file => file.endsWith('.json'))
+        .map(a => `./data/messages/realms/${a}`),
+]) {
     console.log(`   ${file}`);
-    fs.writeFileSync(path.resolve(__dirname, './data/') + '/' + file, JSON.stringify(JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/') + '/' + file))))
+    fs.writeFileSync(path.resolve(__dirname, file), JSON.stringify(JSON.parse(fs.readFileSync(path.resolve(__dirname, file)))))
 }
 
 console.clear();
