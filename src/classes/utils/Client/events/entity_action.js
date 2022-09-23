7f698778ec3151cf6b2b9a3c6240a4563f61231e
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-09-23 14:33:34 +0200

    Made sneaking unsetable

--- src/classes/utils/Client/events/entity_action.js
+++ src/classes/utils/Client/events/entity_action.js
@@ -1,9 +1,9 @@
 module.exports = {
     entity_action: function ({ entityId, actionId, jumpBoost }) {
         if (actionId == 0)
-            this.sneaking = true;
+            require('../properties/public/dynamic/sneaking.js').setPrivate.call(this, true);
         else if (actionId == 1)
-            this.sneaking = false;
+            require('../properties/public/dynamic/sneaking.js').setPrivate.call(this, false);
         else { } //Not implemented            
     }
 }
