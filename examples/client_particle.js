6b0351d20a9a183069b6111cfb7d607564d706a5
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-09-24 21:03:46 +0200

    Updated examples folder

--- examples/client_particle.js
+++ examples/client_particle.js
@@ -1,18 +1,6 @@
-const { Server } = require('minecraft-server')
+const { Server } = require('../')
 const server = new Server()
 
 server.on('join', client => {
-    client.particle(
-        'dust',
-        true,
-        100,
-        client.position,
-        { x: 1, y: 1, z: 1 },
-        {
-            r: 64,
-            g: 73,
-            b: 82
-        },
-        1
-    );
+    client.particle('cloud', true, 100, client.position, { x: 1, y: 1, z: 1 })
 })
--- examples/color_forms.js
+++
@@ -1,7 +0,0 @@
-const { Color } = require('minecraft-server');
-
-//All same color
-const color1 = new Color(20, 30, 80);
-const color2 = new Color({ r: 20, g: 30, b: 80 });
-const color3 = new Color('#141e50');
-const color4 = new Color({ h: 0.3688, s: 0.6, l: 0.2 })
