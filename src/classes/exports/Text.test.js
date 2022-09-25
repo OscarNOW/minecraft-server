7a716abe5880319f46a5b6b598795e53f6331e65
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-09-25 21:15:34 +0200

    Added new setting tests for Text

--- src/classes/exports/Text.test.js
+++ src/classes/exports/Text.test.js
@@ -10,14 +10,6 @@ module.exports = expect => {
     ]), [])
     expect(Text.parseArray([{ text: '', modifiers: ['bold', 'underlined'], color: 'green' }]), [])
 
-    // stringToArray
-    expect(Text.stringToArray(''), [])
-    expect(Text.stringToArray('§r§r§r§r'), [])
-    expect(Text.stringToArray('§r§r§r§r§nH§li§r§a§r'), [
-        { color: 'default', modifiers: ['underlined'], text: 'H' },
-        { color: 'default', modifiers: ['bold', 'underlined'], text: 'i' }
-    ])
-
     // arrayToString
     expect(Text.arrayToString([{ text: 'hi', modifiers: ['bold'], color: 'green' }]), '§r§a§lhi')
     expect(Text.arrayToString([
@@ -32,7 +24,32 @@ module.exports = expect => {
         { text: '3', color: 'default', modifiers: [] },
     ]), '§r§n1§l2§r3');
 
+    // stringToArray
+    expect(Text.stringToArray(''), [])
+    expect(Text.stringToArray('§r§r§r§r'), [])
+    expect(Text.stringToArray('§r§r§r§r§nH§li§r§a§r'), [
+        { color: 'default', modifiers: ['underlined'], text: 'H' },
+        { color: 'default', modifiers: ['bold', 'underlined'], text: 'i' }
+    ])
+
     // parseString
     expect(Text.arrayToString(Text.stringToArray('§r§r§r§r§nH§li§r§a§r§b!§k')), '§r§nH§li§r§b!')
     expect(Text.arrayToString(Text.stringToArray('§r§r§r§r§nH§li§r§a§r')), '§r§nH§li')
+
+    //setting
+    let temp = new Text();
+    temp.string = 'Hello world!';
+    expect(temp.string, '§rHello world!')
+    expect(temp.array, [
+        { text: 'Hello world!', color: 'default', modifiers: [] }
+    ])
+    expect(temp.uncolored, 'Hello world!')
+    expect(temp.chat, 'Hello world!')
+
+    temp = new Text()
+    temp.array = 'Foo bar 123';
+    expect(temp.string, '§rFoo bar 123')
+    expect(temp.array, [{ text: 'Foo bar 123', color: 'default', modifiers: [] }])
+    expect(temp.uncolored, 'Foo bar 123')
+    expect(temp.chat, 'Foo bar 123')
 }
