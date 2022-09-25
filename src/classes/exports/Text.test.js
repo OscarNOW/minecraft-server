413b0b175e628e5222cb92e47d5279bd5886ce5a
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-09-25 20:43:08 +0200

    Cleaned up Text tests

--- src/classes/exports/Text.test.js
+++ src/classes/exports/Text.test.js
@@ -1,37 +1,38 @@
 const Text = require('./Text');
 
 module.exports = expect => {
-    expect(Text.stringToArray(''), [])
+    // parseArray
     expect(Text.parseArray([
         '',
         { text: '', color: 'green', modifiers: ['bold'] },
         { text: '', color: 'green', modifiers: ['bold', 'underlined'] },
         { text: '', color: 'blue', modifiers: [] }
     ]), [])
-    expect(Text.stringToArray('§r§r§r§r'), [])
     expect(Text.parseArray([{ text: '', modifiers: ['bold', 'underlined'], color: 'green' }]), [])
+
+    // stringToArray
+    expect(Text.stringToArray(''), [])
+    expect(Text.stringToArray('§r§r§r§r'), [])
+    expect(Text.stringToArray('§r§r§r§r§nH§li§r§a§r'), [
+        { color: 'default', modifiers: ['underlined'], text: 'H' },
+        { color: 'default', modifiers: ['bold', 'underlined'], text: 'i' }
+    ])
+
+    // arrayToString
     expect(Text.arrayToString([{ text: 'hi', modifiers: ['bold'], color: 'green' }]), '§r§a§lhi')
     expect(Text.arrayToString([
         { text: '1', color: 'default', modifiers: ['underlined'] },
         { text: '2', color: 'default', modifiers: ['underlined', 'bold'] },
         { text: '3', color: 'default', modifiers: [] },
     ]), '§r§n1§l2§r3')
-    expect(Text.arrayToString(Text.stringToArray('§r§r§r§r§nH§li§r§a§r§b!§k')), '§r§nH§li§r§b!')
-
-    let o = new Text([{ text: 'hi', modifiers: ['bold'], color: 'green' }]);
-    expect(o.string, '§r§a§lhi')
-
-    o.array = [
+    expect(Text.arrayToString([{ text: 'hi', modifiers: ['bold'], color: 'green' }]), '§r§a§lhi')
+    expect(Text.arrayToString([
         { text: '1', color: 'default', modifiers: ['underlined'] },
         { text: '2', color: 'default', modifiers: ['underlined', 'bold'] },
         { text: '3', color: 'default', modifiers: [] },
-    ];
-    expect(o.string, '§r§n1§l2§r3')
+    ]), '§r§n1§l2§r3');
 
-    o.string = '§r§r§r§r§nH§li§r§a§r';
-    expect(o.string, '§r§nH§li') //9
-    expect(o.array, [
-        { color: 'default', modifiers: ['underlined'], text: 'H' },
-        { color: 'default', modifiers: ['bold', 'underlined'], text: 'i' }
-    ]) //10
+    // parseString
+    expect(Text.arrayToString(Text.stringToArray('§r§r§r§r§nH§li§r§a§r§b!§k')), '§r§nH§li§r§b!')
+    expect(Text.arrayToString(Text.stringToArray('§r§r§r§r§nH§li§r§a§r')), '§r§nH§li')
 }
