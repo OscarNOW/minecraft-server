d0c9dc7210b9c4bcf4eda673b324178b26fd5360
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-09-29 21:28:09 +0200

    Fixed close window buf

--- src/classes/utils/Client/events/close_window.js
+++ src/classes/utils/Client/events/close_window.js
@@ -3,12 +3,15 @@ const CustomError = require('../../CustomError.js');
 module.exports = {
     close_window: function ({ windowId }) {
         this.p.stateHandler.checkReady.call(this);
-        if (windowId !== this.p.windowId)
+        if (windowId !== this.p.windowId && windowId !== 0)
             this.p.emitError(new CustomError('expectationNotMet', 'client', `windowId in  <remote ${this.constructor.name}>.close_window({ windowId: ${require('util').inspect(windowId)} })  `, {
                 got: windowId,
                 expectationType: 'value',
                 expectation: [this.p.windowId]
             }))
+
+        if (windowId === 0)
+            throw new Error('Not implemented')
 
         this.p.emit('windowClose');
     }
