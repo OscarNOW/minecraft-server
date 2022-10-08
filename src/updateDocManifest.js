c75ee91c49b1a731015efaeaba8c44c3d1e0f39b
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-08 17:04:03 +0200

    Changed updateDocManifest

--- src/updateDocManifest.js
+++ src/updateDocManifest.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
 const path = require('path');
+const newVersion = process.argv[2];
 
 const manifestPath = path.join(__dirname, '../docs/manifest.json');
 
 let manifest = JSON.parse(fs.readFileSync(manifestPath).toString());
+
+if (newVersion !== undefined)
+    manifest.versions.push({
+        version: newVersion,
+        name: newVersion
+    })
+
 delete manifest.versions.filter(({ unstable }) => !unstable)?.find?.(({ latestStable }) => latestStable)?.name;
 delete manifest.versions.filter(({ unstable }) => !unstable)?.find?.(({ latestStable }) => latestStable)?.latestStable;
+
 manifest.versions = [...(manifest.versions.filter(({ unstable }) => !unstable)?.sort((a, b) => compareVersions(a.version, b.version))?.reverse?.() || []), ...(manifest.versions.filter(({ unstable }) => unstable) || [])];
+
 manifest.versions[0].latestStable = true;
 manifest.versions[0].name = `${manifest.versions[0].version} (latest stable)`;
+
 fs.writeFileSync(manifestPath, JSON.stringify(manifest));
 
 function compareVersions(a, b) {
