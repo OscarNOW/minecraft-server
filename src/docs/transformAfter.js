de70bcb6e5dff67089077c117bf0ce6ddc777fcf
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-08 14:32:42 +0200

    Fixed bug now?

--- src/docs/transformAfter.js
+++ src/docs/transformAfter.js
@@ -41,44 +41,7 @@ let versionDropdown = `<label for="versionDropdown" id="versionDropdownLabel">@<
 console.log('Generating version dropdown script...')
 let versionDropdownScript = `
 <script defer>
-
-let cachedVersions;
-async function getVersions() {
-    if (cachedVersions)
-        return cachedVersions;
-
-    cachedVersions = (await getManifest()).versions;
-    return cachedVersions;
-}
-
-let manifest;
-async function getManifest() {
-    if (manifest)
-        return manifest;
-
-    manifest = await fetch('/minecraft-server/\${location.pathname.split('/')[2]}/manifest.json');    
-    manifest = await manifest.json();
-
-    return manifest;
-}
-
-async function versionChange(version) {
-    location = \`/minecraft-server/\${version}/\`
-}
-
-(async () => {
-
-let versions = await getVersions();
-let currentVersion = location.pathname.split('/')[2];
-
-currentVersion = versions.find(({ name }) => name === currentVersion);
-versions = versions.filter(({ name }) => name !== currentVersion.name);
-versions = [currentVersion, ...versions];
-
-const versionDropdown = document.getElementById('versionDropdown');
-versionDropdown.innerHTML = versions.map(({ name, path }) => \`<option value="\${path ?? name}">\${name}</option>\`).join('')
-
-})()
+${fs.readFileSync(path.resolve(__dirname, './versionDropdownScript.js')).toString()}
 </script>
 `;
 
---
+++ src/docs/versionDropdownScript.js
@@ -0,0 +1,40 @@
+/*eslint-env browser */
+/*eslint-disable no-unused-vars*/
+
+let cachedVersions;
+async function getVersions() {
+    if (cachedVersions)
+        return cachedVersions;
+
+    cachedVersions = (await getManifest()).versions;
+    return cachedVersions;
+}
+
+let manifest;
+async function getManifest() {
+    if (manifest)
+        return manifest;
+
+    manifest = await fetch(`/minecraft-server/${location.pathname.split('/')[2]}/manifest.json`);
+    manifest = await manifest.json();
+
+    return manifest;
+}
+
+async function versionChange(version) {
+    location = `/minecraft-server/${version}/`
+}
+
+(async () => {
+
+    let versions = await getVersions();
+    let currentVersion = location.pathname.split('/')[2];
+
+    currentVersion = versions.find(({ name }) => name === currentVersion);
+    versions = versions.filter(({ name }) => name !== currentVersion.name);
+    versions = [currentVersion, ...versions];
+
+    const versionDropdown = document.getElementById('versionDropdown');
+    versionDropdown.innerHTML = versions.map(({ name, path }) => `<option value="${path ?? name}">${name}</option>\`).join('')`)
+
+})();
