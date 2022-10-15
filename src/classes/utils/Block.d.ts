79db2d7ac21b6c9d8033dcc32db387d324be11cc
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-15 13:01:15 +0200

    Added readonly to Block class properties

--- src/classes/utils/Block.d.ts
+++ src/classes/utils/Block.d.ts
@@ -1,7 +1,7 @@
 export class Block {
-    x: number;
-    y: number;
-    z: number;
+    readonly x: number;
+    readonly y: number;
+    readonly z: number;
 
     block: blockType;
     state: blockState;
