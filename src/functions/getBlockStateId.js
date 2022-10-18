0e95154ed595ef1f4a199cd761f8b426a1ef3ae1
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-18 14:13:51 +0200

    Fixed error typo

--- src/functions/getBlockStateId.js
+++ src/functions/getBlockStateId.js
@@ -8,7 +8,7 @@ function getBlockStateId(blockName, state = {}, { function: func } = {}) {
     let stateIds = [];
     for (const { name, values } of block[2]) {
         if (values.indexOf(state[name]) === -1)
-            throw new CustomError('expectationNotMet', 'libraryUser', `stateValue in   <${this.constructor.name}>.${func}(..., ${require('util').inspect(blockName)}, { ${name}: stateValue })  `, [true, false].sort().join(',') == values.sort().join(',') ? {
+            throw new CustomError('expectationNotMet', 'libraryUser', `stateValue in   <${this.constructor.name}>.${func}(${require('util').inspect(blockName)}, ..., { ${name}: stateValue })  `, [true, false].sort().join(',') == values.sort().join(',') ? {
                 got: state[name],
                 expectationType: 'type',
                 expectation: 'boolean'
