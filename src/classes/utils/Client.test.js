1e4e87a1e5246bb98351bdd3e2228d7617f87952
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-21 23:01:40 +0200

    Fixed bug

--- src/classes/utils/Client.test.js
+++ src/classes/utils/Client.test.js
@@ -5,24 +5,39 @@ const Server = require('../exports/Server.js');
 const ProxyClient = require('../exports/ProxyClient.js');
 
 module.exports = (expect, warn) => {
-    const server = new Server();
-    const proxyClient = new ProxyClient();
+    return new Promise(res => {
+        const server = new Server();
+        const proxyClient = new ProxyClient();
 
-    server.on('connect', client => {
-        // for (const test of
-        //     fs
-        //         .readdirSync(path.resolve(__dirname, './Client/properties/public/dynamic/'))
-        //         .filter(v => v.endsWith('.test.js'))
-        //         .map(v => require(`./Client/properties/public/dynamic/${v}`))
-        // ) {
-        //     test({ expect, warn, server, proxyClient, client });
-        //     proxyClient.removeAllListeners();
-        // }
+        server.on('connect', client => {
+            for (const test of
+                fs
+                    .readdirSync(path.resolve(__dirname, './Client/properties/public/dynamic/'))
+                    .filter(v => v.endsWith('.test.js'))
+                    .map(v => require(`./Client/properties/public/dynamic/${v}`))
+            ) {
+                test({ expect, warn, server, proxyClient, client });
+                proxyClient.removeAllListeners();
+            }
 
-        server.close();
-        console.log('server closed');
-    });
+            server.close();
 
-    server.joinProxyClient(proxyClient);
+            res();
+        });
 
+        proxyClient.onPacket(name => {
+            if (name == 'login')
+                proxyClient.sendPacket('settings', {
+                    locale: 'en_us',
+                    viewDistance: 10,
+                    chatFlags: 0,
+                    chatColors: true,
+                    skinParts: 127,
+                    mainHand: 1
+                })
+        })
+
+        server.joinProxyClient(proxyClient);
+
+    })
 }
