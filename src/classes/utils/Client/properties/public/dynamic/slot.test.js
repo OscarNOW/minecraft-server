582f41da46196dcbb48b3043775a353cabde60cf
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-21 23:02:10 +0200

    Created slot test

---
+++ src/classes/utils/Client/properties/public/dynamic/slot.test.js
@@ -0,0 +1,41 @@
+module.exports = ({ expect, client, proxyClient }) => {
+    client.slot = 5;
+
+    expect(client.slot, 5);
+
+    let sentPackets = [];
+    proxyClient.onPacket((name, packet) => sentPackets.push({ name, packet }));
+
+    sentPackets = [];
+    client.slot = 6;
+
+    expect(client.slot, 6);
+    expect(sentPackets.find(v => v.name == 'slot'), {
+        slot: 6
+    });
+
+    sentPackets = [];
+    client.slot = '2';
+
+    expect(client.slot, 2);
+    expect(sentPackets.find(v => v.name == 'slot'), {
+        slot: 2
+    });
+
+    sentPackets = [];
+    client.slot = 11;
+
+    expect(client.slot, 2);
+    expect(sentPackets.find(v => v.name == 'slot'), {
+        slot: 2
+    });
+
+    sentPackets = [];
+    client.slot = '12';
+
+    expect(client.slot, 3);
+    expect(sentPackets.find(v => v.name == 'slot'), {
+        slot: 3
+    });
+
+}
