3c79f0de0948aedee79f2b8382a2944d274b04a4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-23 16:19:37 +0200

    Implemented food test

---
+++ src/classes/utils/Client/properties/public/dynamic/food.test.js
@@ -0,0 +1,29 @@
+module.exports = ({ expect, client, proxyClient }) => {
+    client.food = 5;
+
+    expect(client.food, 5);
+
+    let sentPackets = [];
+    proxyClient.onPacket((name, packet) => sentPackets.push({ name, packet }));
+
+    sentPackets = [];
+    client.food = 6;
+
+    expect(client.food, 6);
+    expect(sentPackets.find(({ name }) => name == 'update_health').packet, {
+        health: client.health,
+        food: 6,
+        foodSaturation: client.foodSaturation
+    });
+
+    sentPackets = [];
+    client.food = '15';
+
+    expect(client.food, 15);
+    expect(sentPackets.find(({ name }) => name == 'update_health').packet, {
+        health: client.health,
+        food: 15,
+        foodSaturation: client.foodSaturation
+    });
+
+}
