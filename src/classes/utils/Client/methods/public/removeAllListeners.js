1a9fe0132824206c858b5e61311c1457d3145d36
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-24 13:07:50 +0200

    Added changeEvent to removeAllListeners

--- src/classes/utils/Client/methods/public/removeAllListeners.js
+++ src/classes/utils/Client/methods/public/removeAllListeners.js
@@ -1,7 +1,15 @@
 module.exports = function (event) {
     if (event)
-        this.p.events[event] = [];
-    else
+        if (event == 'change')
+            for (const type of Object.keys(this.p.changeEvents))
+                this.p.changeEvents[type] = [];
+        else
+            this.p.events[event] = [];
+    else {
         for (const event of Object.keys(this.p.events))
             this.p.events[event] = [];
+
+        for (const type of Object.keys(this.p.changeEvents))
+            this.p.changeEvents[type] = [];
+    }
 }
