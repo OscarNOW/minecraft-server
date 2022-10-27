e30b49c219aea4007796d4af85ba1a2bd7e49e3c
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-27 19:35:50 +0200

    Improved client_particle example

--- examples/client_particle.js
+++ examples/client_particle.js
@@ -1,6 +1,6 @@
 const { Server } = require('@boem312/minecraft-server')
 const server = new Server()
 
-server.on('join', client => {
+server.on('connect', client => {
     client.particle('cloud', true, 100, client.position, { x: 1, y: 1, z: 1 })
 })
