ce5407051f93eb79d6af1aa86da4df04f522d973
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-27 19:34:20 +0200

    Improved server_simple example

--- examples/server_simple.js
+++ examples/server_simple.js
@@ -3,9 +3,7 @@ const server = new Server();
 
 server.on('connect', client => {
 
-    client.on('chat', message => console.log(`<${client.username}> ${message}`));
-
     console.log(`${client.username} joined`);
-    client.chat('Welcome to the server!')
+    client.on('chat', message => console.log(`<${client.username}> ${message}`));
 
 })
