c2c36904e449222fe2f1ab2390d050f5a14988b2
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-10-28 17:43:15 +0200

    Made bossBars changed check better

--- src/classes/utils/Client/properties/public/dynamic/bossBars.js
+++ src/classes/utils/Client/properties/public/dynamic/bossBars.js
@@ -7,7 +7,9 @@ module.exports = {
             return values.get(this);
         },
         setPrivate: function (value) {
-            const changed = value !== this.bossBars;
+            const changed =
+                value.length !== this.bossBars.length ||
+                value.some((a, i) => a !== this.bossBars[i]);
 
             values.set(this, value);
 
