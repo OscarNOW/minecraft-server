9150119c69a5cdea67d5955c3f916ca9bcaf1d16
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-06 17:06:10 +0100

    Improved error handling in build

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,34 @@
 const wait = ms => new Promise(res => setTimeout(res, ms));
+const colors = {
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m",
+
+    fg: {
+        black: "\x1b[30m",
+        red: "\x1b[31m",
+        green: "\x1b[32m",
+        yellow: "\x1b[33m",
+        blue: "\x1b[34m",
+        magenta: "\x1b[35m",
+        cyan: "\x1b[36m",
+        white: "\x1b[37m",
+    },
+    bg: {
+        black: "\x1b[40m",
+        red: "\x1b[41m",
+        green: "\x1b[42m",
+        yellow: "\x1b[43m",
+        blue: "\x1b[44m",
+        magenta: "\x1b[45m",
+        cyan: "\x1b[46m",
+        white: "\x1b[47m"
+    }
+}
 
 const jobs = [
     updateVersionFiles,
@@ -19,7 +49,7 @@ async function executeJobs(jobs) {
     let promises = jobs.map(job => {
         let update = () => {
             updateProgress(jobs.indexOf(job), promiseStates);
-            printProgress();
+            printProgress(errors);
         };
 
         let promise = job(update)
@@ -38,15 +68,13 @@ async function executeJobs(jobs) {
     });
 
     await Promise.all(promises);
-
-    printErrors(errors);
 }
 
 function updateProgress(ind, promiseStates) {
     if (promiseStates[ind] == 'resolved')
-        currentProgress[ind] = '✓';
+        currentProgress[ind] = `${colors.fg.green}✓`;
     else if (promiseStates[ind] == 'rejected')
-        currentProgress[ind] = '✗';
+        currentProgress[ind] = `${colors.fg.red}✗`;
     else if (promiseStates[ind] == 'pending') {
         const progressIndex = progressTypes.indexOf(currentProgress[ind]);
 
@@ -54,15 +82,13 @@ function updateProgress(ind, promiseStates) {
     }
 }
 
-function printProgress() {
+function printProgress(errors) {
     const maxJobNameLength = jobs.reduce((max, job) => Math.max(max, job.name.length), 0);
 
     console.clear()
     for (const i in jobs)
-        console.log(`${jobs[i].name}:${' '.repeat(maxJobNameLength - jobs[i].name.length)} [${currentProgress[i].repeat(10)}]`);
-}
+        console.log(`${jobs[i].name}:${' '.repeat(maxJobNameLength - jobs[i].name.length)} [${currentProgress[i].repeat(10)}${colors.reset}]`);
 
-function printErrors(errors) {
     if (errors.length > 0) {
         console.log('')
         console.log('Errors:')
@@ -101,4 +127,6 @@ async function typeDocs(update) {
     await wait(1250);
     clearInterval(int);
     update();
+
+    throw 1;
 }
