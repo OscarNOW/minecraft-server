bfcaec127d45e186f7cc4aff29980b7eb8647a52
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-08 16:13:53 +0100

    Added showRespawnScreen tests

---
+++ src/classes/utils/Client/properties/public/dynamic/showRespawnScreen.test.js
@@ -0,0 +1,47 @@
+module.exports = ({ expect, client, proxyClient }) => {
+    client.showRespawnScreen = false;
+
+    expect(client.showRespawnScreen, false);
+
+    let sentChangeEvents = [];
+    client.on('change', 'showRespawnScreen', v => sentChangeEvents.push(v))
+
+    let sentPackets = [];
+    proxyClient.onPacket((name, packet) => sentPackets.push({ name, packet }));
+
+    sentChangeEvents = [];
+    sentPackets = [];
+    client.showRespawnScreen = true;
+
+    expect(client.showRespawnScreen, true);
+    expect(sentChangeEvents, [true]);
+    expect(!!sentPackets.find(({ name, packet: { reason, gameMode } }) =>
+        name == 'game_state_change' &&
+        reason == 11 &&
+        gameMode == 0
+    ), true);
+
+    sentChangeEvents = [];
+    sentPackets = [];
+    client.showRespawnScreen = false;
+
+    expect(client.showRespawnScreen, false);
+    expect(sentChangeEvents, [false]);
+    expect(!!sentPackets.find(({ name, packet: { reason, gameMode } }) =>
+        name == 'game_state_change' &&
+        reason == 11 &&
+        gameMode == 1
+    ), true); //
+
+    sentChangeEvents = [];
+    sentPackets = [];
+    client.showRespawnScreen = false;
+
+    expect(client.showRespawnScreen, false);
+    expect(sentChangeEvents, []);
+    expect(!!sentPackets.find(({ name, packet: { reason, gameMode } }) =>
+        name == 'game_state_change' &&
+        reason == 11 &&
+        gameMode == 1
+    ), true); //
+}
