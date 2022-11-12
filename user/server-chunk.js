6601c7e12d9a016351e05e8cacf4d2ca11d854f4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-12 21:40:25 +0100

    Moved user proxyClient into separate file

--- user/server-chunk.js
+++ user/server-chunk.js
@@ -1,28 +1,10 @@
-const { Server, Chunk, ProxyClient } = require('../');
+const { Server, Chunk } = require('../');
 
 let chunk = new Chunk();
 for (let x = 0; x < 16; x++)
     for (let z = 0; z < 16; z++)
         for (let y = 0; y < 100; y++)
             chunk.setBlock('dirt', { x, y, z })
-
-let proxyClient = new ProxyClient();
-proxyClient.onPacket((name, packet) => {
-    if (name === 'map_chunk')
-        console.log('<-', name, '...');
-    else
-        console.log('<-', name, packet);
-
-    if (name === 'login')
-        proxyClient.sendPacket('settings', {
-            locale: 'en_US',
-            viewDistance: 8,
-            chatFlags: 0,
-            chatColors: true,
-            skinParts: 0,
-            mainHand: 0
-        })
-})
 
 const server = new Server({
     defaultClientProperties: () => ({
@@ -37,11 +19,15 @@ const server = new Server({
 
 console.log('Listening')
 server.on('connect', client => {
-    // for (let x = -5; x < 5; x++)
-    //     for (let z = -5; z < 5; z++)
-    //         client.chunk(chunk, { x, z });
+    for (let x = -5; x < 5; x++)
+        for (let z = -5; z < 5; z++)
+            client.chunk(chunk, { x, z });
 
-    client.on('misbehavior', a => console.log(a.toString()))
+    let cow = client.entity('cow', {
+        x: 3,
+        y: 100,
+        z: 3
+    });
+
+    cow.on('leftClick', () => console.log(1))
 })
-
-server.joinProxyClient(proxyClient);
---
+++ user/server-proxyClient.js
@@ -0,0 +1,37 @@
+const { Server, ProxyClient } = require('../');
+
+let proxyClient = new ProxyClient();
+proxyClient.onPacket((name, packet) => {
+    if (name === 'map_chunk')
+        console.log('<-', name, '...');
+    else
+        console.log('<-', name, packet);
+
+    if (name === 'login')
+        proxyClient.sendPacket('settings', {
+            locale: 'en_US',
+            viewDistance: 8,
+            chatFlags: 0,
+            chatColors: true,
+            skinParts: 0,
+            mainHand: 0
+        })
+})
+
+const server = new Server({
+    defaultClientProperties: () => ({
+        gamemode: 'creative',
+        position: {
+            x: 5,
+            y: 101,
+            z: 5
+        }
+    })
+});
+
+console.log('Listening')
+server.on('connect', client => {
+    client.on('misbehavior', a => console.log(a.toString()))
+})
+
+server.joinProxyClient(proxyClient);
