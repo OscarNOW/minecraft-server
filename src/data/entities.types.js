048d6dccc3016298b0ef7b8d74de5e601f6327e5
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-12 22:12:54 +0100

    Created normalEntityName type

--- src/data/entities.types.js
+++ src/data/entities.types.js
@@ -1,5 +1,16 @@
 const { convertToType } = require('../functions/convertToType.js');
+const customEntityClasses = [
+    {
+        class: 'Horse',
+        minecraft: 'horse'
+    },
+    {
+        class: 'Boat',
+        minecraft: 'boat'
+    }
+];
 
 module.exports = {
-    entityName: convertToType(Object.keys(require('./entities.json')))
+    entityName: convertToType(Object.keys(require('./entities.json'))),
+    normalEntityName: convertToType(Object.keys(require('./entities.json')).filter(a => !customEntityClasses.find(({ minecraft }) => minecraft === a))),
 }
