5a5311c7c74d0880be106d25cc7a3aff5f30d2d4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-12 22:01:33 +0100

    Implemented steer Boat event

--- src/classes/utils/Boat.d.ts
+++ src/classes/utils/Boat.d.ts
@@ -1,3 +1,17 @@
 type Entity = import('./Entity').Entity;
 
-export class Boat extends Entity { }
+export class Boat extends Entity {
+
+    removeAllListeners(event?: 'steer' | 'leftClick' | 'rightClick'): void;
+
+    on(event: 'steer', callback: (steerInfo: {
+        left: boolean;
+        right: boolean;
+    }) => void): void;
+
+    once(event: 'steer', callback: (steerInfo: {
+        left: boolean;
+        right: boolean;
+    }) => void): void;
+
+}
--- src/classes/utils/Boat.js
+++ src/classes/utils/Boat.js
@@ -1,5 +1,18 @@
 const Entity = require('./Entity.js');
 
-class Boat extends Entity { }
+const events = Object.freeze([
+    'steer'
+]);
+
+class Boat extends Entity {
+    constructor(client, type, id, position, sendPacket) {
+        super(client, type, id, position, sendPacket);
+
+        this.p.events = {
+            ...this.p.events,
+            ...Object.fromEntries(events.map(a => [a, []]))
+        }
+    }
+}
 
 module.exports = Boat;
