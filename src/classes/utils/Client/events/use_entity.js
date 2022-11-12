e5434e5f9243df22916d9318aa459104a83f2c27
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-12 21:08:16 +0100

    Cleaned up use_entity event

--- src/classes/utils/Client/events/use_entity.js
+++ src/classes/utils/Client/events/use_entity.js
@@ -12,10 +12,16 @@ module.exports = {
             }, null, { server: this.server, client: this }));
 
         require('../properties/public/dynamic/sneaking.js').sneaking.setPrivate.call(this, sneaking);
-        let emitter = this.entities[target] instanceof Client ? this.entities[target].p :
-            this.entities[target] instanceof Entity ? this.entities[target].privateEmitter :
-                this.entities[target].prototype instanceof Entity ? this.entities[target].privateEmitter :
-                    this.entities[target];
+        let emitter;
+
+        if (this.entities[target] instanceof Client)
+            emitter = this.entities[target].p
+        else if (this.entities[target] instanceof Entity)
+            emitter = this.entities[target].privateEmitter
+        else if (this.entities[target].prototype instanceof Entity)
+            emitter = this.entities[target].privateEmitter
+        else
+            emitter = this.entities[target];
 
         if (mouse === 2) {
             if (hand !== 0 && hand !== 1)
