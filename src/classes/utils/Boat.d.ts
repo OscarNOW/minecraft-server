719a881b0f548d18fff6d86f2826bc6e971855b6
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-13 17:22:38 +0100

    Made build work

--- src/classes/utils/Boat.d.ts
+++ src/classes/utils/Boat.d.ts
@@ -13,26 +13,4 @@ export class Boat extends Entity {
         left: boolean;
         right: boolean;
     }) => void): void;
-
-    //inherited by Entity
-    on(event: 'leftClick', callback: () => void): void;
-    on(event: 'rightClick', callback: (clickInfo: {
-        position: {
-            x: number;
-            y: number;
-            z: number;
-        };
-        isMainHand: boolean
-    }) => void): void;
-
-    once(event: 'leftClick', callback: () => void): void;
-    once(event: 'rightClick', callback: (clickInfo: {
-        position: {
-            x: number;
-            y: number;
-            z: number;
-        };
-        isMainHand: boolean
-    }) => void): void;
-
 }
