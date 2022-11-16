17f696c7ebcf5487f3409e13833e5b8ba0432760
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-16 13:43:04 +0100

    Added warn to <Client>.raw(

--- src/classes/utils/Client/methods/public/raw.js
+++ src/classes/utils/Client/methods/public/raw.js
@@ -1,3 +1,4 @@
 module.exports = function (name, packet) {
+    console.warn('This method is for testing purposes only. Do not use this in your code. It will be removed in the future.');
     this.p.client.write(name, packet)
 }
