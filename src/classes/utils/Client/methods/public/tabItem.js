117dc321955c35b9551ce064371eae624ef22d51
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-21 11:13:29 +0100

    Created user file

--- src/classes/utils/Client/methods/public/tabItem.js
+++ src/classes/utils/Client/methods/public/tabItem.js
@@ -2,8 +2,5 @@ const TabItem = require('../../../TabItem.js');
 
 module.exports = function (tabItemOptions) {
     //todo: check input and emit CustomError if not correct
-
-    let tabItem = new TabItem(tabItemOptions, this, this.p.sendPacket);
-
-    return tabItem;
+    return new TabItem(tabItemOptions, this, this.p.sendPacket);
 }
