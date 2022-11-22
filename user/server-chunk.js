c1b1ff7f8ad702bb26cb908627b3fb3398fef33e
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-22 19:15:39 +0100

    Updated user file

--- user/server-chunk.js
+++ user/server-chunk.js
@@ -1,4 +1,5 @@
 const { Server, Chunk } = require('../');
+const wait = ms => new Promise(res => setTimeout(res, ms));
 
 let chunk = new Chunk();
 for (let x = 0; x < 16; x++)
@@ -18,22 +19,32 @@ const server = new Server({
 });
 
 console.log('Listening')
-server.on('connect', client => {
+server.on('connect', async client => {
     for (let x = -5; x < 5; x++)
         for (let z = -5; z < 5; z++)
             client.chunk(chunk, { x, z });
 
-    client.tabItem()
-    console.log(client.tabItems)
+    await client.tabItem({
+        name: 'Jeroen64',
+        uuid: '57c28f3e-47f6-4b2d-9b32-1ce0e078f813',
+        skinAccountUuid: '57c28f3e-47f6-4b2d-9b32-1ce0e078f813'
+    });
 
-    // client.raw('named_entity_spawn', {
-    //     entityId: 10,
-    //     playerUUID: '57c28f3e-47f6-4b2d-9b32-1ce0e078f813',
-    //     x: 3,
-    //     y: 101,
-    //     z: 3,
-    //     yaw: 0,
-    //     pitch: 0
-    // })
+    client.raw('named_entity_spawn', {
+        entityId: 10,
+        playerUUID: '57c28f3e-47f6-4b2d-9b32-1ce0e078f813', //Jeroen64
+        x: 3,
+        y: 101,
+        z: 3,
+        yaw: 0,
+        pitch: 0
+    });
 
-})
+    // client.raw('player_info', {
+    //     action: 4,
+    //     data: [{
+    //         UUID: '57c28f3e-47f6-4b2d-9b32-1ce0e078f813', //Jeroen64
+    //     }]
+    // });
+
+});
