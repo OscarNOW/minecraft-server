2b952d50c61a6b4a694bd977901b8777388d9280
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-11-22 21:34:04 +0100

    Fixed bug in applyDefaults

--- src/functions/applyDefaults.js
+++ src/functions/applyDefaults.js
@@ -1,6 +1,7 @@
 function applyDefaults(properties, defaults) {
     if (properties === undefined) return defaults;
-    if (typeof properties !== 'object') return properties;
+    if (typeof properties !== 'object' || defaults === null) return properties;
+    if (typeof defaults !== 'object' || defaults === null) return properties;
 
     for (const key of Object.keys(defaults))
         properties[key] = applyDefaults(properties[key], defaults[key])
