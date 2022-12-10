7864b7bb92f5f0e7b2639a6c3069b6cb54d9c668
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-10 19:24:12 +0100

    Removed not needed user files

--- user/dataConversion.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-let oldItems = require('../src/data/items.json')
-let newItems = []
-
-for (const [key, value] of Object.entries(oldItems))
-    newItems.push([key, value.id])
-
-fs.writeFileSync('./src/data/items.json', JSON.stringify(newItems))
