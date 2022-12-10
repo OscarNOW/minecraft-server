580ed48269f90e908ebd104ff1412defd5559880
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-10 22:08:59 +0100

    Added debug log to sendPacket

--- src/classes/utils/Client/methods/private/sendPacket.js
+++ src/classes/utils/Client/methods/private/sendPacket.js
@@ -1,5 +1,8 @@
 module.exports = {
     sendPacket(name, packet) {
+        if (name === 'player_info')
+            console.log(packet.data[0])
+
         this.p.client.write(name, packet);
     }
 }
