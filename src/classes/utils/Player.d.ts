54c41dc1e71805c2b199728c74995b8eb529e780
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-11 19:48:51 +0100

    Created new types for Player class

---
+++ src/classes/utils/Player.d.ts
@@ -0,0 +1,10 @@
+export class Player extends Entity {
+    tabItem?: TabItem;
+    playerInfo: playerInfo;
+}
+
+type playerInfo = {
+    readonly name: string; //todo: make changable
+    readonly uuid: string; //todo: make changable
+    gamemode: gamemode;
+}
