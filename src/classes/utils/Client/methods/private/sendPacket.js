e5d50815fbb46dfbb519bf8e3046b7a9830bdb92
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-11 19:49:00 +0100

    Removed debug log from sendPacket

--- src/classes/utils/Client/methods/private/sendPacket.js
+++ src/classes/utils/Client/methods/private/sendPacket.js
@@ -1,8 +1,5 @@
 module.exports = {
     sendPacket(name, packet) {
-        if (name === 'player_info')
-            console.log(packet.data[0])
-
         this.p.client.write(name, packet);
     }
 }
