4928aac38a5b2213be93fe6497b648bccf396063
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-15 19:04:56 +0100

    Cleaned up sendPacket

--- src/classes/utils/Client/methods/private/sendPacket.js
+++ src/classes/utils/Client/methods/private/sendPacket.js
@@ -1,7 +1,7 @@
 module.exports = {
     sendPacket(name, packet) {
         // if (['entity_destroy', 'named_entity_spawn', 'player_info'].includes(name))
-        // console.log(name, require('util').inspect(packet, { depth: 100, colors: true }))
+        //     console.log(name, require('util').inspect(packet, { depth: 100, colors: true }))
 
         this.p.client.write(name, packet);
     }
