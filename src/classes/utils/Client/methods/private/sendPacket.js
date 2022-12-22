eb1ed7819af7ccc849501a1269c27f7c72ec5f90
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-22 10:55:44 +0100

    Updated user files

--- src/classes/utils/Client/methods/private/sendPacket.js
+++ src/classes/utils/Client/methods/private/sendPacket.js
@@ -1,7 +1,7 @@
 module.exports = {
     sendPacket(name, packet) {
         if (name === 'login')
-            console.log(name, packet)
+            console.log(name, packet) //todo-imp: remove
 
         this.p.client.write(name, packet);
     }
