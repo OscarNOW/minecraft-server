61a74f93854326a7290914c900f6af323593f1c6
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-23 16:48:04 +0100

    Removed debug log

--- src/classes/utils/Client/methods/private/sendPacket.js
+++ src/classes/utils/Client/methods/private/sendPacket.js
@@ -1,8 +1,5 @@
 module.exports = {
     sendPacket(name, packet) {
-        if (name === 'login')
-            console.log(name, packet) //todo-imp: remove
-
         this.p.client.write(name, packet);
     }
 }
