ef2aac6325c03d31943514ac4eb942dad2174e10
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-23 20:44:28 +0100

    Improved test with time

--- user/speed/test.js
+++ user/speed/test.js
@@ -1,16 +1,16 @@
-console.time('load')
-
+console.time('loading ')
 const { Server, Chunk } = require('../../');
-console.log('Loaded library')
+console.timeEnd('loading ')
 
+console.time('chunk   ')
 let chunk = new Chunk();
 for (let x = 0; x < 16; x++)
     for (let z = 0; z < 16; z++)
         for (let y = 0; y < 100; y++)
             chunk.setBlock('dirt', { x, y, z })
+console.timeEnd('chunk   ')
 
-console.log('Created chunk')
-
+console.time('server  ')
 const server = new Server({
     defaultClientProperties: () => ({
         gamemode: 'creative',
@@ -21,15 +21,12 @@ const server = new Server({
         }
     })
 });
+console.timeEnd('server  ')
 
-console.log('Created server')
-
+console.time('listen  ')
 server.on('connect', async client => {
     for (let x = -5; x < 5; x++)
         for (let z = -5; z < 5; z++)
             client.chunk(chunk, { x, z });
 });
-
-console.log('Listening')
-
-console.timeEnd('load')
+console.timeEnd('listen  ')
