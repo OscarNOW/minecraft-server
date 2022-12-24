9ee51f0bab904cf0b75453b022845bf17930a179
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-24 14:00:02 +0100

    Fixed bug in stateHandler

--- src/classes/utils/Client/events/end.js
+++ src/classes/utils/Client/events/end.js
@@ -1,8 +1,5 @@
 module.exports = {
     end: function () {
-        if (!this.p.stateHandler.checkReady.call(this))
-            return;
-
         this.p.shutdown()
     }
 }
--- src/classes/utils/Client/methods/private/shutdown.js
+++ src/classes/utils/Client/methods/private/shutdown.js
@@ -1,7 +1,11 @@
 module.exports = {
     shutdown() {
-        this.p.stateHandler.updateState.close.call(this);
+        if (this.p.state !== 'offline')
+            this.p.stateHandler.updateState.close.call(this);
         for (const interval of this.p.intervals) clearInterval(interval);
         for (const timeout of this.p.timeouts) clearInterval(timeout);
+
+        this.p.intervals = [];
+        this.p.timeouts = [];
     }
 }
