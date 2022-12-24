1d0c64b77a240051caa01f5b2d91a96b91e273da
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-24 13:52:16 +0100

    Changed todo in <Client>.chunk(

--- src/classes/utils/Client/methods/public/chunk.js
+++ src/classes/utils/Client/methods/public/chunk.js
@@ -1,13 +1,13 @@
 const LoadedChunk = require('../../../LoadedChunk.js');
 const { chunks } = require('../../properties/public/dynamic/chunks.js');
 
-module.exports = function (chunk, { x, z }) { //todo-time: very slow (from 1s to 3s for multiple chunks)
+module.exports = function (chunk, { x, z }) {
     if (!this.p.stateHandler.checkReady.call(this))
         return;
 
-    const loadedChunk = new LoadedChunk(this, chunk, { x, z }, this.p.sendPacket);
+    const loadedChunk = new LoadedChunk(this, chunk, { x, z }, this.p.sendPacket); //todo-time: takes long, only create loadedChunk when <Client>.chunks is accessed
 
-    this.p.sendPacket('map_chunk', {
+    this.p.sendPacket('map_chunk', { //todo-time: takes long, maybe implement cache so that chunk and getMask only have to be called once
         x,
         z,
         groundUp: true,
@@ -20,7 +20,7 @@ module.exports = function (chunk, { x, z }) { //todo-time: very slow (from 1s to
         bitMap: loadedChunk.chunk.getMask(),
         chunkData: loadedChunk.chunk.dump(),
         blockEntities: []
-    })
+    });
 
     chunks.setPrivate.call(this, Object.freeze([loadedChunk, ...this.chunks]));
 }
