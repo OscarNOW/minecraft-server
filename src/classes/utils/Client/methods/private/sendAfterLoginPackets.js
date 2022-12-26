e3645e74742c4c210d80730213ba1476ff0163d4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-26 14:24:00 +0100

    Fixed bug in sendAfterLoginPackets

--- src/classes/utils/Client/methods/private/sendAfterLoginPackets.js
+++ src/classes/utils/Client/methods/private/sendAfterLoginPackets.js
@@ -1,11 +1,10 @@
 module.exports = {
     sendAfterLoginPackets() {
-        for (const callback of
+        for (const [file, value] of
             Object.entries(this.p.defaultProperties)
                 .map(([name, value]) => [this.p.pubDynProperties[name], value])
-                .filter(([file]) => file.info?.callAfterLogin)
-                .map(([file, value]) => () => file.setRaw.call(this, value, true))
+                .filter(([file, value]) => [file.info?.callAfterLogin, value])
         )
-            callback();
+            file.setRaw.call(this, value, true)
     }
 }
