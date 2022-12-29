401026b26b48514f580de52128f70e74c8ab71b2
Oscar <56079903+OscarNOW@users.noreply.github.com>
2022-12-29 12:00:07 +0100

    Added todo

--- src/classes/utils/Client/methods/private/sendAfterLoginPackets.js
+++ src/classes/utils/Client/methods/private/sendAfterLoginPackets.js
@@ -5,6 +5,6 @@ module.exports = {
                 .map(([name, value]) => [this.p.pubDynProperties[name], value])
                 .filter(([file, value]) => [file.info?.callAfterLogin, value])
         )
-            file.set.call(this, value, true);
+            file.set.call(this, value, true); //todo: some properties only have setPrivate, maybe check and use if present?
     }
 }
