96c2d7217ccb416bee1e766061bedb2726c6967f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-01 22:05:00 +0100

    Fixed bug in languages.types.js

--- src/data/languages.types.js
+++ src/data/languages.types.js
@@ -5,6 +5,6 @@ module.exports = {
     langCode: convertToType(Object.keys(languages)),
     langEnglishName: convertToType(Object.values(languages).map(({ englishName }) => englishName)),
     langMenuName: convertToType(Object.values(languages).map(({ menuName }) => menuName)),
-    langVersion: convertToType(Object.values(languages).map(({ version }) => version)),
-    langRegion: convertToType(Object.values(languages).map(({ region }) => region))
+    langVersion: convertToType(Object.values(languages).map(({ version }) => version).filter(a => a !== undefined)),
+    langRegion: convertToType(Object.values(languages).map(({ region }) => region).filter(a => a !== undefined))
 }
