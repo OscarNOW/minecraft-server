4a1385133902a517072c9a638f8e79b5ef92c1ca
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-01 22:03:18 +0100

    Fixed bug in convertToType

--- src/functions/convertToType.js
+++ src/functions/convertToType.js
@@ -1,6 +1,20 @@
 module.exports = Object.freeze({
     convertToType: (values) => {
-        let type = values.map(a => ['number', 'boolean'].includes(typeof a) ? a : `'${a}'`).sort().join('|');
+        let type = values.map(a => {
+            if (['number', 'boolean'].includes(typeof a))
+                return a;
+
+            if (!a.includes("'"))
+                return `'${a}'`;
+            else if (!a.includes('"'))
+                return `"${a}"`;
+            else if (!a.includes('`'))
+                return `\`${a}\``;
+            else
+                return `'${a.replaceAll("'", "\\'")}'`;
+
+        }).sort().join('|');
+
         if (type === ['true', 'false'].sort().join('|'))
             type = 'boolean'
 
