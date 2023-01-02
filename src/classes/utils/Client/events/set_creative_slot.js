92003ad2316b615b99808fde2160c3e5c9d8c224
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-02 17:23:18 +0100

    Updated new types of Slot

--- src/classes/utils/Client/events/set_creative_slot.js
+++ src/classes/utils/Client/events/set_creative_slot.js
@@ -4,7 +4,7 @@ const { inventory } = require('../properties/public/dynamic/inventory.js');
 module.exports = {
     set_creative_slot({ slot, item: { present, itemId, itemCount, nbtData } }) {
         if (present)
-            inventory.setSlot.call(this, slot, new Slot({ id: itemId, count: itemCount, nbt: nbtData }));
+            inventory.setSlot.call(this, slot, new Slot({ id: itemId, amount: itemCount, nbt: nbtData }));
         else
             inventory.setSlot.call(this, slot, undefined);
     }
