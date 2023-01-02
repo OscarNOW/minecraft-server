fe7af36db41772022a68e343c249a6f0a4044ddb
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-02 16:29:37 +0100

    Added debug to receivePacket

--- src/classes/utils/Client/methods/private/receivePacket.js
+++ src/classes/utils/Client/methods/private/receivePacket.js
@@ -1,5 +1,7 @@
 module.exports = {
     receivePacket(name, packet) {
+        console.log(name)
+
         if (this.p.mpEvents?.[name])
             for (const { callback } of this.p.mpEvents[name])
                 callback(packet);
