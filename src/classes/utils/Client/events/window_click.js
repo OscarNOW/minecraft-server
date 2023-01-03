dcd48219fae12280dca91b5002021f73d52bde1e
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-03 15:37:30 +0100

    Updated window_click event

--- src/classes/utils/Client/events/window_click.js
+++ src/classes/utils/Client/events/window_click.js
@@ -9,11 +9,12 @@ module.exports = {
     // window_click({ windowId, slot: clickedSlotId, mouseButton, mode, item: claimedClickedSlot }) {
     window_click({ windowId, slot: clickedSlotId, mouseButton, mode }) {
         // todo: implement confirmation (with action)
+        // todo:    reject action on misbehavior event
         // todo: check if claimedClickedSlot is same as clickedSlot and throw error if not
         // todo:    sometimes claimedClickedSlot is empty, but clickedSlot is not
-        // todo: only works for clicks in inventory when only inventory is open
         // todo: emit events when certain things happen (like clicking on a slot or outside window)
         // todo: move all to separate files (in folder structure)
+        // todo: only works for clicks in inventory when only inventory is open
 
         if (windowId === 0) { // inventory
             const clickedSlot = clickedSlotId === -999 ?
@@ -23,47 +24,66 @@ module.exports = {
             const cursorSlot = inventory.getSlot.call(this, cursorSlotId);
             const stackable = Slot.stackable(clickedSlot, cursorSlot);
 
-            if (mode === 0) { // normal click
-                if (mouseButton === 0) { // left mouse click
-                    if (stackable) {
+            if (mode === 0) { // click
+                if (clickedSlotId === -999) { // click outside window
+                    if (mouseButton === 0) { // left mouse click
 
-                        // stack both slots
-                        const { stack, rest } = Slot.stack(clickedSlot, cursorSlot);
-                        inventory.setSlot.call(this, cursorSlotId, stack);
-                        inventory.setSlot.call(this, clickedSlotId, rest);
+                        // drop whole cursor slot
+                        // let oldCursorSlot = cursorSlot;
+                        inventory.setSlot.call(this, cursorSlotId, undefined);
+                        // todo: drop oldCursorSlot
 
-                    } else {
+                    } else if (mouseButton === 1) { // right mouse click
 
-                        // swap slots
-                        let oldCursorSlot = cursorSlot;
-                        inventory.setSlot.call(this, cursorSlotId, clickedSlot);
-                        inventory.setSlot.call(this, clickedSlotId, oldCursorSlot);
+                        // drop one from cursorSlot
+                        const { slot1: newCursorSlot } = Slot.moveOne(cursorSlot, Slot.newEmpty());
+                        // const { slot1: newCursorSlot, slot2: dropSlot } = Slot.moveOne(cursorSlot, Slot.newEmpty());
+                        inventory.setSlot.call(this, cursorSlotId, newCursorSlot);
+                        // todo: drop dropSlot
 
                     }
-                } else if (mouseButton === 1) { // right mouse click
-                    if (stackable) {
-                        if (cursorSlot === undefined) {
+                } else { // click on slot
+                    if (mouseButton === 0) { // left mouse click
+                        if (stackable) {
 
-                            // move half of clicked slot to cursor slot
-                            const { bigger, smaller } = Slot.split(clickedSlot);
-                            inventory.setSlot.call(this, cursorSlotId, smaller);
-                            inventory.setSlot.call(this, clickedSlotId, bigger);
+                            // stack both slots
+                            const { stack, rest } = Slot.stack(clickedSlot, cursorSlot);
+                            inventory.setSlot.call(this, cursorSlotId, stack);
+                            inventory.setSlot.call(this, clickedSlotId, rest);
 
                         } else {
 
-                            // drop one from cursor slot to clicked slot
-                            const { slot1: newCursorSlot, slot2: newClickedSlot } = Slot.moveOne(cursorSlot, clickedSlot);
-                            inventory.setSlot.call(this, cursorSlotId, newCursorSlot);
-                            inventory.setSlot.call(this, clickedSlotId, newClickedSlot);
+                            // swap slots
+                            let oldCursorSlot = cursorSlot;
+                            inventory.setSlot.call(this, cursorSlotId, clickedSlot);
+                            inventory.setSlot.call(this, clickedSlotId, oldCursorSlot);
 
                         }
-                    } else {
+                    } else if (mouseButton === 1) { // right mouse click
+                        if (stackable) {
+                            if (cursorSlot === undefined) {
 
-                        // swap slots, same as left click
-                        let oldCursorSlot = cursorSlot;
-                        inventory.setSlot.call(this, cursorSlotId, clickedSlot);
-                        inventory.setSlot.call(this, clickedSlotId, oldCursorSlot);
+                                // move half of clicked slot to cursor slot
+                                const { bigger, smaller } = Slot.split(clickedSlot);
+                                inventory.setSlot.call(this, cursorSlotId, smaller);
+                                inventory.setSlot.call(this, clickedSlotId, bigger);
 
+                            } else {
+
+                                // drop one from cursor slot to clicked slot
+                                const { slot1: newCursorSlot, slot2: newClickedSlot } = Slot.moveOne(cursorSlot, clickedSlot);
+                                inventory.setSlot.call(this, cursorSlotId, newCursorSlot);
+                                inventory.setSlot.call(this, clickedSlotId, newClickedSlot);
+
+                            }
+                        } else {
+
+                            // swap slots, same as left click
+                            let oldCursorSlot = cursorSlot;
+                            inventory.setSlot.call(this, cursorSlotId, clickedSlot);
+                            inventory.setSlot.call(this, clickedSlotId, oldCursorSlot);
+
+                        }
                     }
                 }
             } else if (mode === 1) { // shift click
@@ -71,28 +91,38 @@ module.exports = {
                     // todo: implemented
                     // todo: how exactly does the Client decide which slots to move to?
                 }
-            } else if (mode === 2) { // number key click
-                //todo: check if mouseButton >= 0 && mouseButton <= 8, and if not, emit misbehavior
+            } else if (mode === 2) { // hotbar change
+                //todo: check if (mouseButton >= 0 && mouseButton <= 8) || (mouseButton === 45), and if not, emit misbehavior
 
-                const numberKeySlotId = mouseButton + 36; // 36 is the first slot in the hotbar
-                const numberKeySlot = inventory.getSlot.call(this, numberKeySlotId);
+                if (mouseButton === 45) { // offhand swap
 
-                if (cursorSlot !== undefined) { } // nothing happens if cursor slot is not empty
-                else {
+                    // can't get Client to execute offhand swap, so reject action and emit misbehavior
+                    // todo: emit misbehavior
+                    // todo: reject action
 
-                    // swap clicked slot with number key slot
-                    let oldNumberKeySlot = numberKeySlot;
-                    inventory.setSlot.call(this, numberKeySlotId, clickedSlot);
-                    inventory.setSlot.call(this, clickedSlotId, oldNumberKeySlot);
+                } else { // number key click
+
+                    const numberKeySlotId = mouseButton + 36; // 36 is the first slot in the hotbar
+                    const numberKeySlot = inventory.getSlot.call(this, numberKeySlotId);
+
+                    if (cursorSlot !== undefined) { } // nothing happens if cursor slot is not empty
+                    else {
+
+                        // swap clicked slot with number key slot
+                        let oldNumberKeySlot = numberKeySlot;
+                        inventory.setSlot.call(this, numberKeySlotId, clickedSlot);
+                        inventory.setSlot.call(this, clickedSlotId, oldNumberKeySlot);
+
+                    }
 
                 }
             } else if (mode === 3) { // middle mouse click
                 // not possible in inventory
                 // todo: emit misbehavior
             } else if (mode === 4) { // drop
-                if (clickedSlotId === -999) { // click outside window
+                if (clickedSlotId === -999) { // click outside window without cursor slot
                     // todo: emit event
-                } else { // click on slot
+                } else { // key click while hovering slot
                     if (mouseButton === 0) { // drop key click
                         if (cursorSlot !== undefined) { } // nothing happens if cursor slot is not empty
                         else if (clickedSlot === undefined) { } // nothing happens if clicked slot is empty
