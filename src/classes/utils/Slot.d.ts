895c994fa5c19dbcbc69695df9e23f40d9195fcc
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-04 21:48:05 +0100

    Implemented Slot.stack and Slot.moveOne

--- src/classes/utils/Slot.d.ts
+++ src/classes/utils/Slot.d.ts
@@ -6,17 +6,19 @@ export class Slot {
         nbt: unknown //todo: make known
     });
 
-    private readonly id: number;
+    readonly empty: boolean;
 
-    readonly amount: number;
-    readonly name: itemName;
-    readonly maxStackSize: boolean;
-    readonly displayName: itemDisplayName;
+    private readonly id?: number;
+
+    readonly amount?: number;
+    readonly name?: itemName;
+    readonly maxStackSize?: number;
+    readonly displayName?: itemDisplayName;
 
     static newEmpty(): Slot;
     static stackable(slot1: Slot, slot2: Slot): boolean;
 
-    static stack(slot1: Slot, slot2: Slot): { stack: Slot, rest: Slot };
+    static stack(from: Slot, to: Slot): { stack: Slot, rest: Slot };
     static split(slot: Slot): { bigger: Slot, smaller: Slot };
-    static moveOne(slot1: Slot, slot2: Slot): { slot1: Slot, slot2: Slot }; // todo: make return type array?
+    static moveOne(from: Slot, to: Slot): { from: Slot, to: Slot }; //todo: make generalized method for any amount
 }
--- src/classes/utils/Slot.js
+++ src/classes/utils/Slot.js
@@ -1,36 +1,80 @@
 const { items } = require('../../functions/loader/data.js');
 
 class Slot {
-    //id could be -1, when Slot is empty
     // constructor({ id, amount, nbt }) {
     constructor({ id, amount }) {
-        this.id = id;
-        this.amount = amount;
+        if (id === -1 || amount === 0)
+            this.empty = true;
+        else
+            this.empty = false;
 
-        let itemInfo = items.find(item => item[0] === this.id);
+        if (!this.empty) {
+            this.id = id;
+            this.amount = amount;
 
-        this.name = itemInfo[2];
-        this.displayName = itemInfo[1];
-        this.maxStackSize = itemInfo[3];
+            let itemInfo = items.find(item => item[0] === this.id);
+
+            this.name = itemInfo[2];
+            this.displayName = itemInfo[1];
+            this.maxStackSize = itemInfo[3];
+        };
     };
 
-    // create new Slot each time
-    static newEmpty() { }; // todo: see types for implementation
+    static newEmpty() {
+        return new Slot({ amount: 0 });
+    };
 
-    // when at least one of slots is undefined, they're stackable
-    static stackable() { }; // todo: see types for implementation
+    static stackable(slot1, slot2) {
+        if (slot1.empty || slot2.empty)
+            return true;
+
+        if (slot1.id === slot2.id)
+            return true;
+
+        return false;
+    };
 
     // return new Slots, do not modify original
-    // when at least one is undefined, use newEmpty
-    static stack() { }; // todo: see types for implementation
+    static stack(from, to) {
+        if (!Slot.stackable(from, to))
+            return [from, to];
+
+        const itemsAvailableOnStack = to.maxStackSize - to.amount;
+        let moveAmount;
+        if (from.amount < itemsAvailableOnStack)
+            moveAmount = from.amount;
+        else
+            moveAmount = itemsAvailableOnStack;
+
+        const { from: newFrom, to: newTo } = move(from, to, moveAmount);
+        return { stack: newTo, rest: newFrom };
+
+    };
 
     // return new Slot, do not modify original
     // when Slot is undefined, use newEmpty
     static split() { }; // todo: see types for implementation
 
     // return new Slots, do not modify original
-    // when at least one is undefined, use newEmpty
-    static moveOne() { }; // todo: see types for implementation
+    // use move function
+    static moveOne(from, to) {
+        if (!Slot.stackable(from, to))
+            return [from, to];
+
+        const { from: newFrom, to: newTo } = move(from, to, 1);
+        return { from: newFrom, to: newTo };
+    };
 };
+
+// move amount from from to to, return new Slots without modifying originals
+function move(from, to, amount) {
+    if (from.amount < amount)
+        throw new Error('Trying to move more items than available.') // todo: emit CustomError
+
+    const newFrom = new Slot({ id: from.id, amount: from.amount - amount });
+    const newTo = new Slot({ id: to.id, amount: to.amount + amount });
+
+    return { from: newFrom, to: newTo };
+}
 
 module.exports = Slot;
