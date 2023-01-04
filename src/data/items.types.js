51dae19585fc6a96ed49b88db0ff70d2f3df6b78
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-04 21:16:33 +0100

    Added itemDisplayName

--- src/data/items.types.js
+++ src/data/items.types.js
@@ -1,5 +1,6 @@
 const { convertToType } = require('../functions/convertToType.js');
 
 module.exports = {
-    itemName: convertToType(require('./items.json').map(a => a[0]))
+    itemName: convertToType(require('./items.json').map(a => a[2])),
+    itemDisplayName: convertToType(require('./items.json').map(a => a[1])),
 }
