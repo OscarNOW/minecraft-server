6af8b67b153b47354aa11823c41646897d4f1eb9
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-01-29 15:35:48 +0100

    Fixed prototype pollution in functions/applyDefaults

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Security
+In this release, there are several security fixes. There are (in the previous and new version) currently no known bugs that lead to vulnerabilities, these fixes are just to be safe. It is still recommend however to update to this version. The fixes include:
+- Fixed possible chance of prototype pollution in functions/applyDefaults
+
 ## Existing API changes
 None
 
--- src/functions/applyDefaults.js
+++ src/functions/applyDefaults.js
@@ -3,8 +3,13 @@ function applyDefaults(properties, defaults) {
     if (typeof properties !== 'object' || defaults === null) return properties;
     if (typeof defaults !== 'object' || defaults === null) return properties;
 
-    for (const key of Object.keys(defaults))
+    for (const key of Object.keys(defaults)) {
+        //prevent prototype pollution
+        if (!Object.prototype.hasOwnProperty.call(defaults, key)) continue;
+        if (['__proto__', 'constructor', 'prototype'].includes(key)) continue;
+
         properties[key] = applyDefaults(properties[key], defaults[key])
+    };
 
     return properties;
 }
