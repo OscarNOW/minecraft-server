dc11d6af68fe97f65ce996c9b3393a20b049dc51
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-05 18:12:34 +0100

    Updated user file to convert dat afiles

--- user/format/a.js
+++ user/format/a.js
@@ -1,5 +1,13 @@
-const d = require('../../src/data/blocks.json');
+const p = '../../src/data/entities.json';
+
+const d = require(p);
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFileSync(path.resolve(__dirname, '../../src/data/blocks.json'), JSON.stringify(d))
+let a = Object.entries(d).map(([k, v], i) => ({
+    name: k,
+    living: v.living
+}));
+// console.log(a);
+
+fs.writeFileSync(path.resolve(__dirname, p), JSON.stringify(a))
