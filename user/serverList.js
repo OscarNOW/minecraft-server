139929d7ba1be6d84957fa898dca12db921f8b5c
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-05 21:50:44 +0100

    Updated user file

--- user/serverList.js
+++ user/serverList.js
@@ -2,28 +2,24 @@ const { Server, Text } = require('../');
 
 const server = new Server({
 
-    serverList: ({ ip, connection: { host, port }, version }) => ({
+    serverList: ({ legacy }) => ({
 
-        description: new Text([
-            { text: `Connected through: `, color: 'gray' },
-            { text: `${host}:${port}`, color: 'white', modifiers: ['bold'] },
-            { text: `\nYour ip: `, color: 'gray' },
-            { text: ip, color: 'white', modifiers: ['bold'] }
-        ]),
+        description: [
+            {
+                text: 'Hello, ',
+                color: 'red',
+                modifiers: ['bold', 'italic']
+            },
+            {
+                text: 'world!',
+                color: 'blue',
+                modifiers: ['underlined', 'strikethrough']
+            }
+        ],
 
         players: {
-            online: server.clients.length,
-            max: Math.floor(Math.random() * 100) + 5,
-            hover: `More\nthan\n1\nline!`
-        },
-
-        version: {
-            wrongText: 'Wrong version!',
-
-            /*  Tell client that the correct version is their version, so they
-                always think they have the correct version. Client version is null
-                when the version of the client is unknown                           */
-            correct: version === null ? '1.16.3' : version
+            online: 5,
+            max: 6
         }
 
     })
