208851a82c8ddfb9e653d3873ea7c86847862bb8
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-12 20:47:10 +0100

    Updated user file

--- user/chunk.js
+++ user/chunk.js
@@ -23,7 +23,5 @@ server.on('connect', client => {
         for (let z = -5; z < 5; z++)
             client.chunk(chunk, { x, z });
 
-    client.on('chat', () => {
-        client.gamemode = client.gamemode === 'survival' ? 'creative' : 'survival';
-    });
+    console.log(client.p);
 });
