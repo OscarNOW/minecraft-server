24d0a92b10b8396e1ac6d4a451cb6d25ebf53ee4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-12 21:07:49 +0100

    Removed debug log from receivePacket

--- src/classes/utils/Client/methods/private/receivePacket.js
+++ src/classes/utils/Client/methods/private/receivePacket.js
@@ -1,8 +1,5 @@
 module.exports = {
     receivePacket(name, packet) {
-        if (name === 'window_click')
-            console.log(name, packet)
-
         if (this.p.mpEvents?.[name])
             for (const { callback } of this.p.mpEvents[name])
                 callback(packet);
