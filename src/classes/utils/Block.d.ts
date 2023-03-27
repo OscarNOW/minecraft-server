4b1eb78f3ccb1952ad3d6ec3171476995f94f5e4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-27 13:16:06 +0200

    Fixed types in Block

--- src/classes/utils/Block.d.ts
+++ src/classes/utils/Block.d.ts
@@ -1,12 +1,11 @@
 export class Block {
-    private readonly stateId: number;
-
     readonly x: number;
     readonly y: number;
     readonly z: number;
 
     readonly block: blockName; //todo: rename block to name
     readonly state: blockState;
+    readonly stateId: number;
 }
 
 type blocksSegment = {
