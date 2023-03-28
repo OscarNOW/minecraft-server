151e2904d5da2eef812e2760b3bb6069681f6642
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-28 21:28:51 +0200

    Fixed bug in setBlock

--- src/classes/utils/Client/methods/public/setBlock.js
+++ src/classes/utils/Client/methods/public/setBlock.js
@@ -9,9 +9,14 @@ module.exports = function (block, { x, y, z }, state) {
     if (!chunk)
         return;
 
-    const blockX = x % 16; //todo: use chunkSize instead of 16
-    const blockY = y;
-    const blockZ = z % 16; //todo: use chunkSize instead of 16
+    //todo: mod doesn't work with negative numbers
+    let blockX = x % 16; //todo: use chunkSize instead of 16
+    let blockY = y;
+    let blockZ = z % 16; //todo: use chunkSize instead of 16
+
+    if (blockX < 0) blockX += 16;
+    if (blockY < 0) blockY += 16;
+    if (blockZ < 0) blockZ += 16;
 
     chunk.setBlock(block, { x: blockX, y: blockY, z: blockZ }, state);
 
