0092b46e908760e4b32caf5749f95b1b837aaccc
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-28 19:40:52 +0200

    Created acknowledgeBlockBreak

---
+++ src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
@@ -0,0 +1,30 @@
+const blocks = require('../../../../../data/blocks.json');
+const chunkSize = require('../../../../../data/chunkSize.json');
+
+//todo see todo in block_dig in Client events
+
+module.exports = function ({ x, y, z }, successful) {
+    if (!this.p.stateHandler.checkReady.call(this))
+        return;
+
+    this.p.sendPacket('acknowledge_player_digging', {
+        location: { x, y, z },
+        block:
+            this.chunks.find(({ blocks, x: chunkX, z: chunkZ }) => {
+
+                const xOffset = chunkX * ((chunkSize.x.max - chunkSize.x.min) + 1);
+                const yOffset = 0;
+                const zOffset = chunkZ * ((chunkSize.z.max - chunkSize.z.min) + 1);
+
+                const newX = x - xOffset;
+                const newY = y - yOffset;
+                const newZ = z - zOffset;
+
+                return Boolean(blocks[newX]?.[newY]?.[newZ])
+
+            })?.blocks?.[x]?.[y]?.[z]?.stateId ??
+            blocks.find(([name]) => name === 'air')[1],
+        status: 2,
+        successful
+    })
+}
