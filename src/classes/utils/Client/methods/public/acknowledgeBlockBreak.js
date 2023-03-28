a4615057c22c35e2b841a91190f829db9c3d98d4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-28 22:14:34 +0200

    Fixed bug in acknowledgeBlockBreak

--- src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
+++ src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
@@ -1,28 +1,26 @@
 const blocks = require('../../../../../data/blocks.json');
-const chunkSize = require('../../../../../data/chunkSize.json');
-
-//todo see todo in block_dig in Client events
 
 module.exports = function ({ x, y, z }, successful) {
     if (!this.p.stateHandler.checkReady.call(this))
         return;
 
+    const chunkX = Math.floor(x / 16);
+    const chunkZ = Math.floor(z / 16);
+
+    const chunk = this.chunks.find(({ x, z }) => x === chunkX && z === chunkZ);
+
+    let chunkRelativeX = x % 16; //todo: use chunkSize instead of 16
+    let chunkRelativeY = y;
+    let chunkRelativeZ = z % 16; //todo: use chunkSize instead of 16
+
+    if (chunkRelativeX < 0) chunkRelativeX += 16;
+    if (chunkRelativeY < 0) chunkRelativeY += 16;
+    if (chunkRelativeZ < 0) chunkRelativeZ += 16;
+
     this.p.sendPacket('acknowledge_player_digging', {
         location: { x, y, z },
         block:
-            this.chunks.find(({ blocks, x: chunkX, z: chunkZ }) => {
-
-                const xOffset = chunkX * ((chunkSize.x.max - chunkSize.x.min) + 1);
-                const yOffset = 0;
-                const zOffset = chunkZ * ((chunkSize.z.max - chunkSize.z.min) + 1);
-
-                const newX = x - xOffset;
-                const newY = y - yOffset;
-                const newZ = z - zOffset;
-
-                return Boolean(blocks[newX]?.[newY]?.[newZ])
-
-            })?.blocks?.[x]?.[y]?.[z]?.stateId ??
+            chunk?.blocks?.[chunkRelativeX]?.[chunkRelativeY]?.[chunkRelativeZ]?.stateId ??
             blocks.find(([name]) => name === 'air')[1],
         status: 2,
         successful
