4e546ec28f94c7d1847c7f7b86f22655fd01e97e
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-29 13:27:25 +0200

    Removed todo from Chunk

--- src/classes/exports/Chunk.d.ts
+++ src/classes/exports/Chunk.d.ts
@@ -14,7 +14,6 @@ export class Chunk {
     };
 
     //todo: add overwrite where you can pass a Block class
-    //todo: add to Client
     updateBlock(block: blockName, chunkRelativeLocation: {
         x: number;
         y: number;
