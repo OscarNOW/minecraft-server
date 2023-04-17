55d1c5c9e6952308b203ea0e5ecb0a5debb85726
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-04-17 21:35:35 +0200

    Used <Client>.blocks in more places

--- src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
+++ src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
@@ -4,12 +4,10 @@ module.exports = function ({ x, y, z }, successful) {
     if (!this.p.stateHandler.checkReady.call(this))
         return;
 
-    const block = this.blocks[x]?.[y]?.[z];
-
     this.p.sendPacket('acknowledge_player_digging', {
         location: { x, y, z },
         block:
-            block?.stateId ??
+            this.blocks[x]?.[y]?.[z]?.stateId ??
             blocks.find(([name]) => name === 'air')[1],
         status: 2,
         successful
--- src/classes/utils/Client/methods/public/acknowledgeDigStart.js
+++ src/classes/utils/Client/methods/public/acknowledgeDigStart.js
@@ -1,4 +1,4 @@
-const { blocks, chunkSize } = require('../../../../../functions/loader/data.js');
+const { blocks } = require('../../../../../functions/loader/data.js');
 
 module.exports = function ({ x, y, z }, successful) {
     if (!this.p.stateHandler.checkReady.call(this))
@@ -7,19 +7,7 @@ module.exports = function ({ x, y, z }, successful) {
     this.p.sendPacket('acknowledge_player_digging', {
         location: { x, y, z },
         block:
-            this.chunks.find(({ blocks, x: chunkX, z: chunkZ }) => {
-
-                const xOffset = chunkX * ((chunkSize.x.max - chunkSize.x.min) + 1);
-                const yOffset = 0;
-                const zOffset = chunkZ * ((chunkSize.z.max - chunkSize.z.min) + 1);
-
-                const newX = x - xOffset;
-                const newY = y - yOffset;
-                const newZ = z - zOffset;
-
-                return Boolean(blocks[newX]?.[newY]?.[newZ])
-
-            })?.blocks?.[x]?.[y]?.[z]?.stateId ??
+            this.blocks[x]?.[y]?.[z]?.stateId ??
             blocks.find(([name]) => name === 'air')[1],
         status: 0,
         successful
