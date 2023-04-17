3227775b887308a3cb90d59291e792252170e6ce
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-04-17 19:02:12 +0200

    Used <Client>.blocks in code

--- src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
+++ src/classes/utils/Client/methods/public/acknowledgeBlockBreak.js
@@ -4,23 +4,12 @@ module.exports = function ({ x, y, z }, successful) {
     if (!this.p.stateHandler.checkReady.call(this))
         return;
 
-    const chunkX = Math.floor(x / 16);
-    const chunkZ = Math.floor(z / 16);
-
-    const chunk = this.chunks.find(({ x, z }) => x === chunkX && z === chunkZ);
-
-    let chunkRelativeX = x % 16; //todo: use chunkSize instead of 16
-    let chunkRelativeY = y;
-    let chunkRelativeZ = z % 16; //todo: use chunkSize instead of 16
-
-    if (chunkRelativeX < 0) chunkRelativeX += 16;
-    if (chunkRelativeY < 0) chunkRelativeY += 16;
-    if (chunkRelativeZ < 0) chunkRelativeZ += 16;
+    const block = this.blocks[x]?.[y]?.[z];
 
     this.p.sendPacket('acknowledge_player_digging', {
         location: { x, y, z },
         block:
-            chunk?.blocks?.[chunkRelativeX]?.[chunkRelativeY]?.[chunkRelativeZ]?.stateId ??
+            block?.stateId ??
             blocks.find(([name]) => name === 'air')[1],
         status: 2,
         successful
