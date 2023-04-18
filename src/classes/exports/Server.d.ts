cd1f3e8376edba3b8f26e86339b012d772526816
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-04-19 00:18:17 +0200

    Fixed indentation in Server types

--- src/classes/exports/Server.d.ts
+++ src/classes/exports/Server.d.ts
@@ -43,43 +43,43 @@ export class Server {
         /**
          * @example serverList: ({ ip }) => ({
          *
-         *      description: `A minecraft server\nYour ip is ${ip}`,
-         *      players: {
-         *          online: server.clients.length,
-         *          max: 100
-         *      },
-         *      version: {
-         *          wrongText: 'Please use 1.16.3'
-         *      }
+         *              description: `A minecraft server\nYour ip is ${ip}`,
+         *              players: {
+         *                  online: server.clients.length,
+         *                  max: 100
+         *              },
+         *              version: {
+         *                  wrongText: 'Please use 1.16.3'
+         *              }
          *
-         * })
+         *         })
          * @example serverList: ({ ip, connection: { host, port }, version }) => ({
          *
-         *      description: new Text([
-         *          { text: 'Connected through: ', color: 'gray' },
-         *          { text: `${host}:${port}`, color: 'white', modifiers: ['bold'] },
-         *          { text: '\nYour ip: ', color: 'gray' },
-         *          { text: ip, color: 'white', modifiers: ['bold'] }
-         *      ]),
+         *              description: new Text([
+         *                  { text: 'Connected through: ', color: 'gray' },
+         *                  { text: `${host}:${port}`, color: 'white', modifiers: ['bold'] },
+         *                  { text: '\nYour ip: ', color: 'gray' },
+         *                  { text: ip, color: 'white', modifiers: ['bold'] }
+         *              ]),
          *
-         *      players: {
-         *          online: server.clients.length + 5,
-         *          max: Math.floor(Math.random() * 100) + 5,
-         *          hover: 'More\nthan\n1\nline!'
-         *      },
+         *              players: {
+         *                  online: server.clients.length + 5,
+         *                  max: Math.floor(Math.random() * 100) + 5,
+         *                  hover: 'More\nthan\n1\nline!'
+         *              },
          *
-         *      version: {
-         *          wrongText: 'Wrong version!',
+         *              version: {
+         *                  wrongText: 'Wrong version!',
          *
-         *          / *  Tell client that the correct version is their version, so they
-         *              always think they have the correct version. Reported client
-         *              version is null when the version of the client is unknown      * /
-         *          correct: version === null ? '1.16.3' : version
-         *      },
+         *                  / *  Tell client that the correct version is their version, so they
+         *                      always think they have the correct version. Reported client
+         *                      version is null when the version of the client is unknown      * /
+         *                  correct: version === null ? '1.16.3' : version
+         *              },
          *
-         *      favicon: fs.readFileSync('./favicon.png')
+         *              favicon: fs.readFileSync('./favicon.png')
          *
-         *  })
+         *          })
          */
         serverList?(info: {
             ip: string,
