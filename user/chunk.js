aa8c84534e6bce2e53c8f850284a0c43be88d2ed
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-04-19 09:48:48 +0200

    Updated user file

--- user/chunk.js
+++ user/chunk.js
@@ -1,18 +1,17 @@
 const { Server, Chunk } = require('../');
 
 let chunk = new Chunk();
-// for (let x = 0; x < 16; x++)
-//     for (let z = 0; z < 16; z++)
-//         for (let y = 0; y < 100; y++)
-//             chunk.setBlock('dirt', { x, y, z })
-chunk.setBlock('dirt', { x: 1, y: 2, z: 3 })
+for (let x = 0; x < 16; x++)
+    for (let z = 0; z < 16; z++)
+        for (let y = 0; y < 100; y++)
+            chunk.setBlock('dirt', { x, y, z })
 
 const server = new Server({
     defaultClientProperties: () => ({
         gamemode: 'creative',
         position: {
             x: 1,
-            y: 3,
+            y: 100,
             z: 3
         }
     })
@@ -23,9 +22,4 @@ server.on('connect', client => {
     for (let x = -5; x < 5; x++)
         for (let z = -5; z < 5; z++)
             client.chunk(chunk, { x, z });
-
-    client.on('blockBreak', ({ x, y, z }, oldBlock) => {
-        client.updateBlock(oldBlock.block, { x, y, z }, oldBlock.state);
-        client.acknowledgeBlockBreak({ x, y, z }, false);
-    })
 });
