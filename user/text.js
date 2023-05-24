c5bb050faca0ef9e71dab1079c3456f5d6ada6da
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-05-24 22:45:17 +0200

    Updated user file

--- user/text.js
+++ user/text.js
@@ -1,14 +1,18 @@
 const { Text } = require('../');
 
-let a = new Text({
-    translate: 'demo.day.4',
-    with: [
-        {
-            text: 'Hello',
-            color: 'red'
-        }
-    ]
-})
+let a = new Text([{
+    text: 'true'
+}]);
+
+// let a = new Text({
+//     translate: 'demo.day.4',
+//     with: [
+//         {
+//             text: 'Hello',
+//             color: 'red'
+//         }
+//     ]
+// })
 
 // console.log(require('util').inspect(a.string, { depth: 100, colors: true }))
 // console.log(require('util').inspect(a.array, { depth: 100, colors: true }))
