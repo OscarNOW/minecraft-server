cd544dfef62b9472afbca1ef9ffc7644828ed08e
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-06-14 09:03:13 +0200

    Built

--- docs/github/index.d.ts
+++ docs/github/index.d.ts
@@ -164,7 +164,7 @@ declare class BossBar{constructor(client:Client,sendPacket:(packetName:string,pa
 /**
  * @see https://oscarnow.github.io/minecraft-server/1.4.0/classes/Changable
  */
-declare class Changable{constructor(changeCallback:(values:{[valueName:string|symbol]:unknown;},oldValues:{[valueName:string|symbol]:unknown;})=>void,startValues:{[valueName:string|symbol]:unknown;});setRaw(values:{[valueName:string|symbol]:unknown;}):void;setRaw(key:string|symbol,value:unknown):void;readonly raw:{[valueName:string|symbol]:unknown;};[valueName:string|symbol]:unknown;}
+declare class Changeable{constructor(changeCallback:(values:{[valueName:string|symbol]:unknown;},oldValues:{[valueName:string|symbol]:unknown;})=>void,startValues:{[valueName:string|symbol]:unknown;});setRaw(values:{[valueName:string|symbol]:unknown;}):void;setRaw(key:string|symbol,value:unknown):void;readonly raw:{[valueName:string|symbol]:unknown;};[valueName:string|symbol]:unknown;}
 /**
  * @see https://oscarnow.github.io/minecraft-server/1.4.0/classes/Client
  */
