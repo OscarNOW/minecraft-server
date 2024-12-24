35d1568642a4dd307fa4f42294fc7eca185a43a6
Oscar <56079903+OscarNOW@users.noreply.github.com>
2024-12-24 14:55:40 +0100

    Improved comments on formatJavaString

--- src/functions/formatJavaString.js
+++ src/functions/formatJavaString.js
@@ -22,13 +22,15 @@ module.exports = {
                 if (i === -1)
                     break;
 
+                //todo: use CustomError
                 if (replaceableStringIndex > args.length - 1)
-                    throw new Error(`Not enough arguments for %${code} in "${string}". Got ${args.length}, expected at least ${replaceableStringIndex + 1}`); //todo: use CustomError
+                    throw new Error(`Not enough arguments for %${code} in "${string}". Got ${args.length}, expected at least ${replaceableStringIndex + 1}`);
 
                 const arg = args[replaceableStringIndex];
 
+                //todo: use CustomError and make message clearer
                 if ((!validInputTypes.includes(typeof arg)) && (validInputs && !validInputs.includes(arg)))
-                    throw new Error(`Invalid argument type for %${code} in ${string}`); //todo: use CustomError and make message clearer
+                    throw new Error(`Invalid argument type for %${code} in ${string}`);
 
                 string = string.replace(`%${code}`, arg);
                 replaceableStringIndex++;
