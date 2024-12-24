ffb8297b482ae8448a7a0b062cf82b5ac6ec81f7
Oscar <56079903+OscarNOW@users.noreply.github.com>
2024-12-24 15:02:51 +0100

    Updated user files

--- user/text.js
+++ user/text.js
@@ -1,19 +1,41 @@
 const { Text } = require('../');
 
-let a = new Text([{
-    text: 'true'
-}]);
+// let a = new Text([{
+// text: 'true'
+// }]);
 
-// let a = new Text({
-//     translate: 'demo.day.4',
-//     with: [
-//         {
-//             text: 'Hello',
-//             color: 'red'
-//         }
-//     ]
-// })
+let a = new Text([
+    {
+        translate: 'options.graphics.warning.vendor',
+        color: 'darkGreen',
+        with: [
+            {
+                translate: 'item.durability',
+                color: 'green',
+                modifiers: ['bold'],
+                with: [
+                    {
+                        text: 'Hello',
+                        modifiers: ['bold'],
+                        color: 'red'
+                    },
+                    {
+                        translate: 'options.graphics.warning.vendor',
+                        color: 'purple',
+                        modifiers: ['italic'],
+                        with: [
+                            {
+                                text: 'World',
+                                color: 'blue'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+]);
 
-// console.log(require('util').inspect(a.string, { depth: 100, colors: true }))
 // console.log(require('util').inspect(a.array, { depth: 100, colors: true }))
-console.log(require('util').inspect(a.chat, { depth: 100, colors: true }))
+console.log(require('util').inspect(a.string, { depth: 100, colors: true }))
+// console.log(require('util').inspect(a.chat, { depth: 100, colors: true }))
